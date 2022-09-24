eae25eacb056fa7d7af18a0641884083d5f9f2c9
selman <gunduzgece546@gmail.com>
2022-09-24 18:25:55 +0300

    fetch with fetcher function

--- libs/ui/src/examples/Fetch/E2_FetchFetcher.tsx
+++ libs/ui/src/examples/Fetch/E2_FetchFetcher.tsx
@@ -1,17 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react'
 
-import { Box, Stack } from '@chakra-ui/react'
+import { Box, Stack, Text } from '@chakra-ui/react'
 import { API_URL, TOKEN } from '@wsvvrijheid/config'
 import { fetcher } from '@wsvvrijheid/utils'
+const BLOG_URL = `${API_URL}/api/blogs` // https://api.samenvvv.nl/api/blogs
 
 export const FetchFetcher = () => {
   const [blogs, setBlogs] = useState([])
 
+  const getBlogs = async () => {
+    const response = await fetcher()(BLOG_URL)
+    setBlogs(response.data)
+  }
   useEffect(() => {
     // TODO: fetch blogs with fetcher by using the API_URL and TOKEN
     // Remember that fetcher is a wrapper around axios that adds the token and api url to the request
+    getBlogs()
   }, [])
 
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+  return (
+    <Box>
+      {/* TODO: Show only title of the blogs */}
+
+      {blogs.map(blog => {
+        return <Text fontSize="xl">{blog['title']}</Text>
+      })}
+    </Box>
+  )
 }
