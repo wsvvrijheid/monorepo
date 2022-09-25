20ff31e0fc39e6027ca499b24bd2248251769879
selman <gunduzgece546@gmail.com>
2022-09-25 13:19:38 +0300

    fetch with request done

--- libs/ui/src/examples/Fetch/E3_FetchRequest.tsx
+++ libs/ui/src/examples/Fetch/E3_FetchRequest.tsx
@@ -1,16 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react'
 
-import { Box, Stack } from '@chakra-ui/react'
+import { Box, Stack, Text } from '@chakra-ui/react'
 import { API_URL, TOKEN } from '@wsvvrijheid/config'
 import { request } from '@wsvvrijheid/utils'
 
 export const FetchRequest = () => {
   const [blogs, setBlogs] = useState([])
-
+  const getBlogs = async () => {
+    const { data } = await request({ url: 'api/blogs' })
+    console.log(data)
+    setBlogs(data)
+  }
   useEffect(() => {
     // TODO: fetch blogs with our custom request function
+    getBlogs()
   }, [])
 
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+  return (
+    <Box>
+      {/* TODO: Show only title of the blogs */}
+
+      {blogs.map(blog => {
+        return <Text fontSize="xl">{blog['title']}</Text>
+      })}
+    </Box>
+  )
 }
