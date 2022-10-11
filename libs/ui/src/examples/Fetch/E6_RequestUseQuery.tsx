fc3c7df6c533105e4f6d4acca03f22e70df6e4cc
Ibrahim Akancay <iakancay@gmail.com>
2022-10-11 23:27:14 +0200

    E6 is done

--- libs/ui/src/examples/Fetch/E6_RequestUseQuery.tsx
+++ libs/ui/src/examples/Fetch/E6_RequestUseQuery.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { FC, useEffect, useState } from 'react'
 
-import { Box, HStack, Select } from '@chakra-ui/react'
+import {
+  Alert,
+  AlertIcon,
+  Box,
+  Button,
+  Code,
+  HStack,
+  Input,
+  Select,
+  Spinner,
+} from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
-import { StrapiLocale, StrapiUrl } from '@wsvvrijheid/types'
+import { StrapiLocale, StrapiModel, StrapiUrl } from '@wsvvrijheid/types'
 import { request } from '@wsvvrijheid/utils'
 
 import { LanguageSwitcher } from '../../admin'
@@ -11,10 +21,10 @@ import { URL_OPTIONS } from './RequestPopulate'
 
 export const RequestUseQuery = () => {
   // TODO: Specify the type of all the states
-  const [url, setUrl] = useState('api/blogs')
-  const [filterValue, setFilterValue] = useState('')
-  const [filterProperty, setFilterProperty] = useState('title')
-  const [locale, setLocale] = useState('en')
+  const [url, setUrl] = useState<StrapiUrl>('api/blogs')
+  const [filterValue, setFilterValue] = useState<string>('')
+  const [filterProperty, setFilterProperty] = useState<string>('title')
+  const [locale, setLocale] = useState<StrapiLocale>('en')
 
   // Example filter: { [filterProperty]: filterValue }
   // eg { title: 'test' } for blogs
@@ -22,6 +32,12 @@ export const RequestUseQuery = () => {
 
   const fetchData = async () => {
     // TODO: Add request to fetch data
+    const data = await request<StrapiModel[]>({
+      url,
+      [filterProperty]: filterValue,
+      locale,
+    })
+    return data.data
   }
 
   const { data, isLoading, isError, refetch } = useQuery({
@@ -36,11 +52,47 @@ export const RequestUseQuery = () => {
     <Box>
       <HStack>
         {/* TODO: LanguageSwitcher */}
+        {['en', 'nl', 'tr'].map(lang => (
+          <Button
+            key={lang}
+            onClick={() => {
+              setLocale(lang as StrapiLocale)
+            }}
+          >
+            {lang}
+          </Button>
+        ))}
         {/* TODO: Select with URL_OPTIONS */}
+        <Select
+          placeholder="Select URL options"
+          onChange={e => setUrl(e.target.value as StrapiUrl)}
+        >
+          <option value="/api/blogs">Blogs Url</option>
+          <option value="/api/categories">Category Url</option>
+        </Select>
         {/* TODO: Input for filterProperty */}
+        <Input
+          placeholder="Filter Properties: like title for blogs.."
+          onChange={e => setFilterProperty(e.target.value)}
+        />
         {/* TODO: Input for filterValue */}
+        <Input
+          placeholder="Filter Values: like test for title property"
+          onChange={e => setFilterValue(e.target.value)}
+        />
         {/* TODO: Fetch button to trigger refetch */}
+        <Button onClick={() => refetch()}>Search</Button>
         {/* TODO: Show data */}
+        {isLoading ? (
+          <Spinner size="lg" color="blue.500" />
+        ) : isError ? (
+          <Alert status="error">
+            <AlertIcon />
+            There was an error processing your request
+          </Alert>
+        ) : (
+          <Code>{JSON.stringify(data)}</Code>
+        )}
       </HStack>
     </Box>
   )
