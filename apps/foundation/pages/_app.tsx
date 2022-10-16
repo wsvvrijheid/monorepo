6a5783cf387543b0daf327292a4ec1bb74784796
7alip <talipaltas@gmail.com>
2022-10-16 12:16:19 +0200

    Move head tags to app.tsx

--- apps/foundation/pages/_app.tsx
+++ apps/foundation/pages/_app.tsx
@@ -11,6 +11,7 @@ import { defaultSeo, themes } from '@wsvvrijheid/config'
 import { checkAuth, pageview, store } from '@wsvvrijheid/utils'
 import { appWithTranslation } from 'next-i18next'
 import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { Provider as ReduxProvider } from 'react-redux'
 
@@ -41,6 +42,27 @@ function MyApp({ Component, pageProps }) {
       <Hydrate state={pageProps.dehydratedState}>
         <ReduxProvider store={store}>
           <ChakraProvider theme={themes.wsvvrijheid}>
+            <Head>
+              <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+              />
+              <link rel="manifest" href="/site.webmanifest" />
+            </Head>
             <DefaultSeo {...defaultSeo.wsvvrijheid[router.locale]} />
             <Component {...pageProps} />
             <ToastContainer />
