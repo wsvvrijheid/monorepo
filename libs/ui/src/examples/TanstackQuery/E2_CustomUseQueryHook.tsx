0a6de869763c5a879737278625af242e01cfa873
selman <gunduzgece546@gmail.com>
2022-10-16 10:54:28 +0300

    I studied on customusequery hook

--- libs/ui/src/examples/TanstackQuery/E2_CustomUseQueryHook.tsx
+++ libs/ui/src/examples/TanstackQuery/E2_CustomUseQueryHook.tsx
@@ -1,11 +1,20 @@
 import { Box, Code } from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
+import { Blog } from '@wsvvrijheid/types'
+import { Request } from '@wsvvrijheid/utils'
 
 // TODO: Create custom hook that returns `useQuery` hook
 // TODO: Move queryFn to a separate function called `getBlogs`
 // TODO: You can move your previous code to this custom hook
 const useBlogsQuery = () => {
-  return useQuery({})
+  const getBlogs = async () => {
+    const data = Request.single<Blog>({
+      url: 'api/blogs',
+      locale: 'tr',
+    })
+    return data
+  }
+  return useQuery({ queryKey: ['blogs', 'tr'], queryFn: getBlogs })
 }
 
 export const CustomUseQueryHook = () => {
