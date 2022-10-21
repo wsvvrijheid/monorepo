049bd0384ff1b64678f319ce64907037bd7e4caa
selman <gunduzgece546@gmail.com>
2022-10-21 17:14:37 +0300

    Multifetch query is done

--- libs/ui/src/examples/TanstackQuery/1_MultiFetch/1_MultiFetchQuery.tsx
+++ libs/ui/src/examples/TanstackQuery/1_MultiFetch/1_MultiFetchQuery.tsx
@@ -13,12 +13,19 @@ export const MultiFetchQuery = () => {
   const authorsQuery = useAuthorsQuery()
   const blogsQuery = useBlogsQuery(selectedAuthorId)
 
-  console.log('authorsQuery', authorsQuery)
+  // console.log(selectedAuthorId)
 
   return (
     <Stack>
-      <Select placeholder="Select author">
+      <Select
+        placeholder="Select author"
+        value={selectedAuthorId}
+        onChange={e => setSelectedAuthorId(e.target.value)}
+      >
         {/* TODO: List all the authors and set the selectedAuthorId with onChange method */}
+        {authorsQuery.data?.data.map(author => (
+          <option value={author.id}>{author.username}</option>
+        ))}
       </Select>
       <Code as="pre">{JSON.stringify(blogsQuery.data, null, 2)}</Code>
     </Stack>
--- libs/ui/src/examples/TanstackQuery/1_MultiFetch/blogs.ts
+++ libs/ui/src/examples/TanstackQuery/1_MultiFetch/blogs.ts
@@ -5,6 +5,14 @@ export const getAuthorBlogs = (authorId?: number) => {
   return Request.collection({
     url: 'api/blogs',
     // TODO: Add filters to fetch the blogs of the selected author
+    filters: {
+      author: {
+        id: {
+          $eq: authorId,
+        },
+      },
+    },
+    locale: 'tr',
   })
 }
 
@@ -14,3 +22,13 @@ export const useBlogsQuery = (authorId?: number) => {
     queryFn: () => getAuthorBlogs(authorId),
   })
 }
+
+// const response = await Request.collection<Blog[]>({
+//   url: 'api/blogs',
+//   filters: {
+//     $and: [{ author: { id: { $eq: authorID } } }, { id: { $ne: blogId } }],
+//   },
+//   sort: ['publishedAt:desc'],
+//   pageSize: 2,
+//   locale,
+// })
