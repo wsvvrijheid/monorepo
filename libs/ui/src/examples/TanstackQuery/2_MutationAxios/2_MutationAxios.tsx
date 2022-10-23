fd699d0223fd211e0ed4cf34b23c5ca0997a72f9
selman <gunduzgece546@gmail.com>
2022-10-23 19:47:39 +0300

    mutation axios

--- libs/ui/src/examples/TanstackQuery/2_MutationAxios/2_MutationAxios.tsx
+++ libs/ui/src/examples/TanstackQuery/2_MutationAxios/2_MutationAxios.tsx
@@ -2,110 +2,69 @@
 import { FormEvent, useState } from 'react'
 
 import { Button, Input, SimpleGrid, Skeleton, Stack } from '@chakra-ui/react'
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { Category, CategoryCreateInput, StrapiModel } from '@wsvvrijheid/types'
-import { Mutation, Request, useGetArtCategories } from '@wsvvrijheid/utils'
-
+import { useQuery } from '@tanstack/react-query'
+import { Category } from '@wsvvrijheid/types'
+import { Request } from '@wsvvrijheid/utils'
 
 import { createCategoryWithAxios } from '../createCategoryWithAxios'
-import { Pagination } from 'libs/ui/src/components'
-import { sendStatusCode } from 'next/dist/server/api-utils'
 
 // We will create a new category
 // Category has `name_en`, `name_nl`, `name_tr` and `slug` fields
 
 export const MutationAxios = () => {
-  const [nameEn, setNameEn] = useState<string>("")
-  const [nameTr, setNameTr] = useState<string>("")
-  const [nameNl, setNameNl] = useState<string>("")
-  const [page, setPage] = useState(1)
   // TODO: Create states for the input fields `name_en`, `name_tr` and `name_nl`
+  const [nameEn, setNameEn] = useState<string>('')
+  const [nameTr, setNameTr] = useState<string>('')
+  const [nameNl, setNameNl] = useState<string>('')
 
-  type CreateCategoryFormFields = {
-    name_en: string,
-    name_tr: string,
-    name_nl: string,
-  }
-
-  type CategoryFormFields = Pick<
-    CategoryCreateInput,
-    'name_en' | 'name_nl' | 'name_tr'
-  >
   const { data, isLoading, isFetching } = useQuery({
-    queryKey: ['categories', page],
+    queryKey: ['categories'],
     queryFn: () =>
       Request.collection<Category[]>({
         url: 'api/categories',
-
       }),
   })
-
-  // const createCategoryMutation = useMutation({
-  //   mutationKey: ['create-category'],
-  //   mutationFn: (variables: CreateCategoryFormFields) => {
-  //     const categoryBody = {
-
-  //       name_en: variables.name_en,
-  //       name_tr: variables.name_tr,
-  //       name_nl: variables.name_nl
-  //     }
-  //     return Mutation.post<Category, CategoryFormFields>(
-  //       'api/categories',
-  //       categoryBody,
-
-  //     )
-  //   },
-  //   onSuccess: (data, variables) => {
-
-  //   }
-  // })
-  console.log(nameNl)
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     // TODO: Call the mutation function (createCategoryWithAxios) with the input values from the state
-    const args:CreateCategoryFormFields = {
-      name_en:nameEn,
-      name_tr:nameTr,
-      name_nl:nameNl
-    }
-    createCategoryWithAxios({
+    const res = createCategoryWithAxios({
       name_en: nameEn,
       name_nl: nameTr,
       name_tr: nameNl,
-  })
+    })
+    console.log(res)
   }
 
   return (
-    <SimpleGrid p={8} bg='gray.100' columns={{ base: 1, md: 2 }} gap={8}>
+    <SimpleGrid p={8} bg="gray.100" columns={{ base: 1, md: 2 }} gap={8}>
       <form onSubmit={handleSubmit}>
-        <Stack p={8} bg='white' spacing={4} >
+        <Stack p={8} bg="white" spacing={4}>
           {/* TODO: Add input fields for `name_en`, `name_tr` and `name_nl` */}
           <Input
-            placeholder='name_en'
+            placeholder="name_en"
             onChange={e => setNameEn(e.target.value)}
           />
           <Input
-            placeholder='name_tr'
+            placeholder="name_tr"
             onChange={e => setNameTr(e.target.value)}
           />
           <Input
-            placeholder='name_nl'
+            placeholder="name_nl"
             onChange={e => setNameNl(e.target.value)}
           />
-          <Button colorScheme="primary">Create</Button>
+          <Button colorScheme="primary" type="submit">
+            Create
+          </Button>
         </Stack>
       </form>
-      <Stack p={8} bg='white' spacing={4} >
+      <Stack p={8} bg="white" spacing={4}>
         {data?.data.map((category, i) => (
-          <Skeleton isLoaded={!isLoading && !isFetching} key={i} >
+          <Skeleton isLoaded={!isLoading && !isFetching} key={i}>
             {category.name_en}
           </Skeleton>
         ))}
       </Stack>
-
-
-
     </SimpleGrid>
   )
 }
