24123c778c1314b31a4ffee576bf4607f2d2fad2
zahirekrem09 <zhrekrmsrtk@gmail.com>
2022-10-26 00:35:43 +0200

    Add Cokkie Banner Comp.

---
+++ libs/ui/src/components/CookieBanner/CookieBanner.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react'
+
+import {
+  Button,
+  CloseButton,
+  Icon,
+  Link,
+  Square,
+  Stack,
+  StackProps,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react'
+import { BiCookie } from 'react-icons/bi'
+
+import { Navigate } from '../Navigate'
+
+export type CookieBannerProps = StackProps & {
+  onClose?: () => void
+  onAllow?: () => void
+  onReject?: () => void
+}
+
+export const CookieBanner = (props: CookieBannerProps) => {
+  const isMobile = useBreakpointValue({ base: true, md: false })
+  const { onClose, onAllow, onReject, ...rest } = props
+
+  return (
+    <Stack
+      justify="center"
+      align={'center'}
+      spacing="4"
+      p="4"
+      direction={{ base: 'column', sm: 'row' }}
+      bg="gray.700"
+      rounded={'md'}
+      boxShadow="sm"
+      {...rest}
+    >
+      <CloseButton
+        display={{ sm: 'none' }}
+        position="absolute"
+        right="2"
+        top="2"
+        color={'teal'}
+        onClick={onClose}
+      />
+      {!isMobile && (
+        <Square size="12" bg="lightgray" borderRadius="md">
+          <Icon as={BiCookie} boxSize="6" color={'teal'} />
+        </Square>
+      )}
+      <Text color="white" fontSize={{ base: 'sm', md: 'md' }}>
+        By using our website, you agree to the use of cookies as described in
+        our{' '}
+        <Navigate
+          _hover={{ transform: 'scale(2.1)', color: 'teal' }}
+          as={Link}
+          href="#"
+        >
+          Cookie Policy
+        </Navigate>
+      </Text>
+      <Stack
+        direction={{ base: 'column', sm: 'row' }}
+        spacing={{ base: '3', sm: '2' }}
+        align={{ base: 'stretch', sm: 'center' }}
+        sx={{
+          ...(isMobile && { width: '100%' }),
+        }}
+      >
+        <Button
+          bg="white"
+          color="black"
+          _hover={{ bg: 'gray.100' }}
+          size="sm"
+          flexShrink={0}
+          onClick={onReject}
+        >
+          Reject
+        </Button>
+        <Button colorScheme={'teal'} size="sm" flexShrink={0} onClick={onAllow}>
+          Allow
+        </Button>
+        <CloseButton
+          color={'teal'}
+          display={{ base: 'none', sm: 'inline-flex' }}
+          onClick={onClose}
+        />
+      </Stack>
+    </Stack>
+  )
+}
