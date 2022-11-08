3465bd4cbef346607812146cc34dea8a90202886
Mustafa <cana66331@gmail.com>
2022-11-08 21:27:50 +0100

    feat colums updated

--- libs/ui/src/admin/DataTables/PostsTable/columns.tsx
+++ libs/ui/src/admin/DataTables/PostsTable/columns.tsx
@@ -1,4 +1,10 @@
-import { Post, StrapiLocale, TranslationStatus } from '@wsvvrijheid/types'
+import {
+  ApprovalStatus,
+  Hashtag,
+  Post,
+  StrapiLocale,
+  TranslationStatus,
+} from '@wsvvrijheid/types'
 
 import { WTableProps } from '../../../components'
 import { LocaleBadges } from '../../LocaleBadges'
@@ -8,10 +14,16 @@ export const columns: WTableProps<Post>['columns'] = {
   image: {
     type: 'image',
   },
+  creator: {},
   title: {
     sortable: true,
   },
-
+  content: {},
+  hashtag: {
+    sortable: true,
+    label: 'Hashtag',
+    transform: value => (value as Hashtag)?.title,
+  },
   createdAt: {
     type: 'date',
     componentProps: {
@@ -37,6 +49,20 @@ export const columns: WTableProps<Post>['columns'] = {
   translates: {
     label: 'Locales',
     transform: value => <LocaleBadges locales={value as StrapiLocale[]} />,
+  },
+  approvalStatus: {
+    type: 'badge',
+    componentProps: value => {
+      const colorScheme = {
+        approved: 'green',
+        pending: 'yellow',
+        rejected: 'red',
+      }
+      return {
+        variant: 'outline',
+        colorScheme: colorScheme[value as ApprovalStatus],
+      }
+    },
   },
   publishedAt: {
     label: 'Published',
