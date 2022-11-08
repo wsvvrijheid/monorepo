19a887188ddc47591090d903f5938ddb7fddc337
Mustafa <cana66331@gmail.com>
2022-11-08 19:03:03 +0100

    feat adminav updated, posts page created

--- apps/admin/pages/hashtags/posts.tsx
+++ apps/admin/pages/hashtags/posts.tsx
@@ -1,10 +1,63 @@
-import { Box } from '@chakra-ui/react'
-import { AdminLayout } from '@wsvvrijheid/ui'
+import { useState } from 'react'
+
+import { usePostsByFilterAndSort } from '@wsvvrijheid/services' //usePosts
+import { StrapiLocale, Sort } from '@wsvvrijheid/types'
+import { AdminLayout, MainHashtagTable } from '@wsvvrijheid/ui'
+import { useUpdateEffect } from 'react-use'
 
 const HashtagPostsPage = () => {
+  const [currentPage, setCurrentPage] = useState<number>()
+  const defaultLocale: StrapiLocale = 'en'
+
+  const [searchTerm, setSearchTerm] = useState<string>()
+  const [locale, setLocale] = useState<StrapiLocale>(defaultLocale)
+  const [sort, setSort] = useState<Sort>()
+  const queryKey = ['posts', searchTerm, sort, currentPage || 1]
+  // const getPosts = usePosts()
+  // console.log('get posts all >>>>>>>', getPosts?.data)
+
+  const PostsQuery = usePostsByFilterAndSort(queryKey, {
+    sort,
+    searchTerm,
+    page: currentPage || 1,
+    locale: locale as StrapiLocale,
+  })
+  const handleSearch = (search: string) => {
+    search ? setSearchTerm(search) : setSearchTerm(undefined)
+  }
+
+  useUpdateEffect(() => {
+    PostsQuery.refetch()
+  }, [locale, searchTerm, sort])
+
+  const posts = PostsQuery?.data?.data
+  console.log('posts >>>>>>> ', posts)
+
+  const totalCount = PostsQuery?.data?.meta?.pagination?.pageCount
+  console.log('total count', totalCount)
+
+  const mappedPosts = posts?.map(posts => ({
+    ...posts,
+    translates: posts.localizations?.map(l => l.locale),
+  }))
+  console.log('mappded posts >>>>>>', mappedPosts)
+
   return (
-    <AdminLayout title="HashtagPosts">
-      <Box>HashtagPosts</Box>
+    <AdminLayout
+      title="HashtagPosts"
+      headerProps={{
+        onSearch: handleSearch,
+        onLanguageSwitch: locale => setLocale(locale),
+        defaultLocale,
+      }}
+    >
+      <MainHashtagTable
+        data={mappedPosts}
+        totalCount={totalCount}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        onSort={setSort}
+      />
     </AdminLayout>
   )
 }
---
+++ libs/services/src/post/getByFilterandSort.ts
@@ -0,0 +1,64 @@
+import { useQuery, QueryKey } from '@tanstack/react-query'
+import { Request } from '@wsvvrijheid/lib'
+import { Hashtag, Sort, StrapiLocale } from '@wsvvrijheid/types'
+
+type GetPostsArgs = {
+  populate?: string | string[]
+  page?: number
+  pageSize?: number
+  searchTerm?: string
+  sort?: Sort
+  locale: StrapiLocale
+  publicationState?: 'live' | 'preview'
+}
+
+export const getPostsByFilterAndSort = async ({
+  page = 1,
+  pageSize = 12,
+  searchTerm,
+  sort = ['date:desc'],
+  publicationState = 'preview',
+  locale,
+  populate,
+}: GetPostsArgs) => {
+  const descriptionFilter = {
+    description: {
+      $containsi: searchTerm,
+    },
+  }
+
+  const titleFilter = {
+    title: {
+      $containsi: searchTerm,
+    },
+  }
+
+  const searchFilter = searchTerm && {
+    $or: [descriptionFilter, titleFilter],
+  }
+
+  const filters: { [key: string]: unknown } = {
+    ...(searchFilter || {}),
+  }
+
+  return Request.collection<Hashtag[]>({
+    url: 'api/posts',
+    filters,
+    page,
+    pageSize,
+    locale,
+    sort: sort || undefined,
+    publicationState,
+    populate,
+  })
+}
+
+export const usePostsByFilterAndSort = (
+  queryKey: QueryKey,
+  args: GetPostsArgs,
+) =>
+  useQuery({
+    queryKey,
+    queryFn: () => getPostsByFilterAndSort(args),
+    keepPreviousData: true,
+  })
