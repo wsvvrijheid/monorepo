1fc6019dfc0d31c639bcf5c84d52192770d24ea6
Yousef Kara <yousef@Admins-MBP.hsd1.pa.comcast.net>
2022-11-10 11:27:59 -0500

    backend mention search

---
+++ apps/api/src/api/mention/controllers/custom.ts
@@ -0,0 +1,10 @@
+import { twitterApi } from '../../../libs/twitter/client'
+
+export default {
+  async search(ctx) {
+    const result = await twitterApi.v1.searchUsers(ctx.query.q)
+    ctx.send({ count: result })
+
+    ctx.send(result.data)
+  },
+}
