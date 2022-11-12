4e735143d1146879a48d03d025fda5b78a761afa
7alip <talipaltas@gmail.com>
2022-11-12 14:43:44 +0100

    Update mention search controller

--- apps/api/src/api/mention/controllers/custom.ts
+++ apps/api/src/api/mention/controllers/custom.ts
@@ -3,8 +3,6 @@ import { twitterApi } from '../../../libs/twitter/client'
 export default {
   async search(ctx) {
     const result = await twitterApi.v1.searchUsers(ctx.query.q)
-    ctx.send({ count: result })
-
     ctx.send(result.data)
   },
 }
