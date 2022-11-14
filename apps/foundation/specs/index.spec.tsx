637122554a130cfc4dccf262ebbbec83f71f72d0
7alip <talipaltas@gmail.com>
2022-11-14 08:39:32 +0100

    Refactoring

--- apps/foundation/specs/index.spec.tsx
+++ apps/foundation/specs/index.spec.tsx
@@ -6,7 +6,7 @@ import Index from '../pages/index'
 
 describe('Index', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Index />)
+    const { baseElement } = render(<Index seo={{}} />)
     expect(baseElement).toBeTruthy()
   })
 })
