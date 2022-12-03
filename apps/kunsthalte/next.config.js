6d5c65a64d77ac1afc49e7235e014f81c3fa3822
7alip <talipaltas@gmail.com>
2022-12-03 16:03:24 +0100

    Fix links and image domains

--- apps/kunsthalte/next.config.js
+++ apps/kunsthalte/next.config.js
@@ -10,7 +10,12 @@ const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   i18n,
   images: {
-    domains: ['api.samenvvv.nl', 'kunsthalte.com', 'localhost'],
+    domains: [
+      'api.samenvvv.nl',
+      'wsvv-api.onrender.com',
+      'kunsthalte.com',
+      'localhost',
+    ],
   },
   nx: {
     // Set this to true if you would like to to use SVGR
