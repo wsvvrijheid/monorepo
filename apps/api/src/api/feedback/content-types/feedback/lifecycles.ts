e326f3c36dc173543a7d3dfe051f22c9ef8ef32c
MSKose <saitmustafa1998@gmail.com>
2022-12-08 10:21:43 +0100

    conflicts corected

--- apps/api/src/api/feedback/content-types/feedback/lifecycles.ts
+++ apps/api/src/api/feedback/content-types/feedback/lifecycles.ts
@@ -1,20 +1,26 @@
 export default {
   async afterCreate({ result }) {
-    const populatedArt = await strapi.entityService.findMany('api::feedback.feedback',{
-        filters: {
-          id: {
-            $eq: result.id,
-          },
-        },
-        populate: ['art'],
+    // populating feedback so as to get the art and artist info
+    const populatedFeedback = await strapi.entityService.findOne('api::feedback.feedback', result.id, {
+        populate: ['art.artist'],
       },
     )
 
-    strapi.plugins['email'].services.email.send({
-      to: result.createdBy.email,
-      from: 'info@wsvvrijheid.nl',
-      subject: `Your art '${populatedArt[0].art.title}' application has been ${result.status}`,
-      html: `<p><a href="http://localhost:1337/admin/plugins/content-type-builder/content-types/api::feedback.feedback${result.id}">${result.message}</a></p>`,
-    })
+    if (populatedFeedback.art?.artist) {
+      const art = populatedFeedback.art
+      const artist = art.artist
+
+      strapi.plugins['email'].services.email.send({
+        to: artist.email,
+        from: 'info@wsvvrijheid.nl',
+        subject: `Dear ${artist.name || artist.username} your art "${art.title}" has been ${result.status}`,
+        html: `<div>
+                  <p>Editor note: ${result.message}</p>
+                  <p>View your art in your <a href="${process.env.KUNSTHALTE_SITE_URL}/profile">profile</a></p>
+              </div>`,
+      })
+    } else {
+      return `artist does not exist for ${result.id}`
+    }
   },
-}
+};
