767ba714dbf8a9eb1e5d5941f39a5c76272d3842
selman <gunduzgece546@gmail.com>
2022-12-13 00:25:14 +0300

    add remove bookmark tweet

--- apps/admin/pages/content-maker/tweets-bookmarks.tsx
+++ apps/admin/pages/content-maker/tweets-bookmarks.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from '@chakra-ui/react'
+import { Box, HStack, Link, Text } from '@chakra-ui/react'
 import { AdminLayout, TimelineLocalTweet, TimelineTweet } from '@wsvvrijheid/ui'
 import { useLocalStorage } from 'usehooks-ts'
 
@@ -17,11 +17,55 @@ const TweetBookmarkedPage = () => {
         searchPlaceHolder: 'Search bookmarks',
       }}
     >
-      <SimpleGrid columns={{ base: 1 }} gap={4}>
+      {/* <SimpleGrid columns={{ base: 1 }} gap={4}>
         {tweetBookmarksStorage.map((t, key) => (
           <TimelineTweet tweet={t.tweet} user={t.user} key={key} />
         ))}
-      </SimpleGrid>
+      </SimpleGrid> */}
+      <HStack
+        align="center"
+        bg={'white'}
+        rounded="lg"
+        p={4}
+        gap={4}
+        overflowY="auto"
+        shouldWrapChildren={true}
+        display="flex"
+        justify="center"
+      >
+        <Box
+          w="500px"
+          overflowX="auto"
+          borderRadius="6px"
+          border="1px"
+          borderColor="gray.300"
+        >
+          <Link
+            href={``}
+            target="_blank"
+            rel="noreferrer noopener"
+            cursor="pointer"
+          >
+            <Box bg="blue.400" borderBottom="1px" color="white" p={3}>
+              <HStack>
+                <Text
+                  fontSize={'sm'}
+                  wordBreak={'break-all'}
+                  fontWeight={'bolder'}
+                >
+                  Bookmarked Tweets
+                </Text>
+              </HStack>
+            </Box>
+          </Link>
+
+          <Box overflowY="auto">
+            {tweetBookmarksStorage.map((t, key) => (
+              <TimelineTweet tweet={t.tweet} user={t.user} key={key} />
+            ))}
+          </Box>
+        </Box>
+      </HStack>
     </AdminLayout>
   )
 }
--- libs/ui/src/admin/TimelineTweet/TimelineTweet.tsx
+++ libs/ui/src/admin/TimelineTweet/TimelineTweet.tsx
@@ -15,17 +15,47 @@ import {
 import { BsBookmarkPlus, BsThreeDots } from 'react-icons/bs'
 import { RiEditLine } from 'react-icons/ri'
 import twitterText from 'twitter-text'
+import { useLocalStorage } from 'usehooks-ts'
 
 import { WImage } from '../../components'
-import { TimelineTweetProps } from './types'
+import { TimelineLocalTweet, TimelineTweetProps } from './types'
 
 export const TimelineTweet: FC<TimelineTweetProps> = ({
   tweet,
-  onEdit,
-  onSave,
   user,
   ...rest
 }) => {
+  const [tweetBookmarksStorage, setTweetBookmarksStorage] = useLocalStorage<
+    TimelineLocalTweet[]
+  >('tweetBookmarks', [])
+
+  const isBookmarked = tweetBookmarksStorage?.some(t => t.tweet.id === tweet.id)
+
+  const onEdit = (data: TimelineLocalTweet) => {
+    console.log('edit')
+  }
+
+  const onSave = (data: TimelineLocalTweet) => {
+    const newSavedTweet = data
+
+    if (tweetBookmarksStorage.length > 0) {
+      const filteredBookmarks = tweetBookmarksStorage?.filter(
+        t => t.tweet.id !== data.tweet.id,
+      )
+      if (tweetBookmarksStorage?.some(t => t.tweet.id === data.tweet.id)) {
+        setTweetBookmarksStorage([...filteredBookmarks])
+      } else {
+        setTweetBookmarksStorage([...filteredBookmarks, newSavedTweet])
+      }
+    } else {
+      const newTweetBookmarks = [
+        ...(tweetBookmarksStorage || []),
+        newSavedTweet,
+      ]
+      setTweetBookmarksStorage(newTweetBookmarks)
+    }
+  }
+
   return (
     <HStack
       spacing={4}
@@ -67,10 +97,10 @@ export const TimelineTweet: FC<TimelineTweetProps> = ({
                   Edit
                 </MenuItem>
                 <MenuItem
-                  icon={<BsBookmarkPlus />}
+                  icon={<BsBookmarkPlus color={isBookmarked ? 'red' : ''} />}
                   onClick={() => onSave({ tweet, user })}
                 >
-                  Save (Bookmark)
+                  {isBookmarked ? 'Remove' : 'Save'} (Bookmark)
                 </MenuItem>
               </MenuList>
             </Menu>
