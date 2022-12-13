848b2e280812b3dec266e4233f2b173b2ea77644
selman <gunduzgece546@gmail.com>
2022-12-13 20:15:49 +0300

    requests complated

--- apps/admin/pages/content-maker/tweets-bookmarks.tsx
+++ apps/admin/pages/content-maker/tweets-bookmarks.tsx
@@ -1,27 +1,15 @@
-import { Box, HStack, Link, Text } from '@chakra-ui/react'
+import { Box, HStack, Text } from '@chakra-ui/react'
 import { AdminLayout, TimelineLocalTweet, TimelineTweet } from '@wsvvrijheid/ui'
 import { useLocalStorage } from 'usehooks-ts'
 
 const TweetBookmarkedPage = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [tweetBookmarksStorage, setTweetBookmarksStorage] = useLocalStorage<
-    TimelineLocalTweet[]
-  >('tweetBookmarks', [])
+  const [tweetBookmarksStorage] = useLocalStorage<TimelineLocalTweet[]>(
+    'tweetBookmarks',
+    [],
+  )
 
   return (
-    <AdminLayout
-      title="News"
-      headerProps={{
-        onSearch: () => null,
-        filterMenuCloseOnSelect: false,
-        searchPlaceHolder: 'Search bookmarks',
-      }}
-    >
-      {/* <SimpleGrid columns={{ base: 1 }} gap={4}>
-        {tweetBookmarksStorage.map((t, key) => (
-          <TimelineTweet tweet={t.tweet} user={t.user} key={key} />
-        ))}
-      </SimpleGrid> */}
+    <AdminLayout title="Bookmarked Tweets">
       <HStack
         align="center"
         bg={'white'}
@@ -40,24 +28,17 @@ const TweetBookmarkedPage = () => {
           border="1px"
           borderColor="gray.300"
         >
-          <Link
-            href={``}
-            target="_blank"
-            rel="noreferrer noopener"
-            cursor="pointer"
-          >
-            <Box bg="blue.400" borderBottom="1px" color="white" p={3}>
-              <HStack>
-                <Text
-                  fontSize={'sm'}
-                  wordBreak={'break-all'}
-                  fontWeight={'bolder'}
-                >
-                  Bookmarked Tweets
-                </Text>
-              </HStack>
-            </Box>
-          </Link>
+          <Box bg="blue.400" borderBottom="1px" color="white" p={3}>
+            <HStack>
+              <Text
+                fontSize={'sm'}
+                wordBreak={'break-all'}
+                fontWeight={'bolder'}
+              >
+                Bookmarked Tweets
+              </Text>
+            </HStack>
+          </Box>
 
           <Box overflowY="auto">
             {tweetBookmarksStorage.map((t, key) => (
