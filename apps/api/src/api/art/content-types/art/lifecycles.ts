f51b3605307fa107c11ec86fc569f2d9f8dbca6f
MSKose <saitmustafa1998@gmail.com>
2022-12-18 15:31:47 +0100

    #243 done

--- apps/api/src/api/art/content-types/art/lifecycles.ts
+++ apps/api/src/api/art/content-types/art/lifecycles.ts
@@ -1,10 +1,26 @@
 export default {
-  afterCreate({ result }) {
-    strapi.plugins['email'].services.email.send({
-      to: 'info@wsvvrijheid.nl',
-      from: 'info@wsvvrijheid.nl',
-      subject: `New Art Created ${result.title}`,
-      html: `<p><a href="https://api.wsvvrijheid.nl/admin/content-manager/collectionType/api::art.art${result.id}">${result.title}</a></p>`,
-    })
+  async afterCreate({ result }) {
+    // do not forget to define your comma seperated EDITOR_EMAILS in your local env
+    const editorEmails = process.env.EDITOR_EMAILS?.split(',');
+
+    // populating artist to use in email subject
+    const populatedArtist = await strapi.entityService.findOne('api::art.art', result.id, {
+      populate: ['artist'],
+    },)
+    const artist = populatedArtist.artist
+
+    if (editorEmails?.length > 0) {
+      strapi.plugins['email'].services.email.send({
+        to: editorEmails,
+        from: 'info@wsvvrijheid.nl',
+        subject: `New Art ${result.title} has been created by ${artist.name || artist.username}`,
+        html: `<div>
+                  <p>Description: ${result.description || result.title}</p>
+                  <p>View the art <a href="https://api.wsvvrijheid.nl/admin/content-manager/collectionType/api::art.art/${result.id}">here</a></p>
+              </div>`,
+      })
+    } else {
+      console.log('no editor email exist');
+    }
   },
 }
