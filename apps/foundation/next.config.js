72758263fed7a79e987073caf40ec76308ab3ee0
7alip <talipaltas@gmail.com>
2022-12-19 11:38:23 +0100

    Trigger deploy

--- apps/foundation/next.config.js
+++ apps/foundation/next.config.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx')
-const path = require('path')
 
 const { i18n } = require('./next-i18next.config')
 
