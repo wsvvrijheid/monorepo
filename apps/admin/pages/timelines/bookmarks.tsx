2e2231b5a5c1b3c24940100946dc151f7441969e
7alip <talipaltas@gmail.com>
2023-01-02 15:43:53 +0100

    Update bookmarked timelines page

--- apps/admin/pages/timelines/bookmarks.tsx
+++ apps/admin/pages/timelines/bookmarks.tsx
@@ -1,5 +1,10 @@
-import { Box, HStack, Text } from '@chakra-ui/react'
-import { AdminLayout, TimelineLocalTweet, TimelineTweet } from '@wsvvrijheid/ui'
+import {
+  AdminLayout,
+  Container,
+  MasonryGrid,
+  TimelineLocalTweet,
+  TimelineTweet,
+} from '@wsvvrijheid/ui'
 import { useLocalStorage } from 'usehooks-ts'
 
 const TweetBookmarkedPage = () => {
@@ -10,43 +15,13 @@ const TweetBookmarkedPage = () => {
 
   return (
     <AdminLayout title="Bookmarked Tweets">
-      <HStack
-        align="center"
-        bg={'white'}
-        rounded="lg"
-        p={4}
-        gap={4}
-        overflowY="auto"
-        shouldWrapChildren={true}
-        display="flex"
-        justify="center"
-      >
-        <Box
-          w="500px"
-          overflowX="auto"
-          borderRadius="6px"
-          border="1px"
-          borderColor="gray.300"
-        >
-          <Box bg="blue.400" borderBottom="1px" color="white" p={3}>
-            <HStack>
-              <Text
-                fontSize={'sm'}
-                wordBreak={'break-all'}
-                fontWeight={'bolder'}
-              >
-                Bookmarked Tweets
-              </Text>
-            </HStack>
-          </Box>
-
-          <Box overflowY="auto">
-            {tweetBookmarksStorage.map((t, key) => (
-              <TimelineTweet tweet={t.tweet} user={t.user} key={key} />
-            ))}
-          </Box>
-        </Box>
-      </HStack>
+      <Container>
+        <MasonryGrid cols={[1, 1, 1, 2, 3]}>
+          {tweetBookmarksStorage.map((t, key) => (
+            <TimelineTweet tweet={t.tweet} user={t.user} key={key} />
+          ))}
+        </MasonryGrid>
+      </Container>
     </AdminLayout>
   )
 }
