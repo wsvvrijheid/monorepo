1a1eff8a71d9421e3e867b1a86181e1f84ce1210
7alip <talipaltas@gmail.com>
2023-01-03 07:07:37 +0100

    Fix required fields for validation

--- libs/ui/src/data/schemas/activity.ts
+++ libs/ui/src/data/schemas/activity.ts
@@ -4,18 +4,18 @@ import { FormFields } from '../../admin'
 
 export const activitySchema = yup.object({
   title: yup.string().required('Title is required'),
+  date: yup.date().required('Date is required'),
   description: yup.string().required('Description is required'),
   content: yup.string().required('Content is required'),
-  date: yup.date().required('Date is required'),
-  image: yup.mixed(),
+  image: yup.mixed().required('Image is required'),
 })
 
 export type ActivitySchemaKeys = keyof yup.InferType<typeof activitySchema>
 
 export const activityFields: FormFields<ActivitySchemaKeys> = [
   { name: 'title', isRequired: true },
+  { name: 'date', isRequired: true, type: 'datetime-local' },
   { name: 'description', isRequired: true, type: 'textarea' },
   { name: 'content', isRequired: true, type: 'textarea' },
-  { name: 'date', isRequired: true, type: 'datetime-local' },
   { name: 'image', type: 'file', isRequired: true },
 ]
