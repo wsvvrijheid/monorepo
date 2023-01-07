7eec9db659ff048caae1567aa9c272f5b0804eaa
7alip <talipaltas@gmail.com>
2023-01-08 00:48:59 +0100

    Add MdFormItem

--- libs/ui/src/components/MarkdownEditor/index.ts
+++ libs/ui/src/components/MarkdownEditor/index.ts
@@ -1,1 +1,7 @@
-export * from './MarkdownEditor'
+import dynamic from 'next/dynamic'
+
+export const MarkdownEditor = dynamic(() => import('./MarkdownEditor'), {
+  ssr: false,
+})
+
+export * from './MdFormItem'
