0c8b4faf53b8cd80db634e5b8a0676d3cad85b07
MustafaTRKYLMZ <cana66331@gmail.com>
2023-01-16 20:43:03 +0100

    icon buttons added

--- libs/ui/src/admin/RecommendedTweetCard/RecommendedTweetCard.tsx
+++ libs/ui/src/admin/RecommendedTweetCard/RecommendedTweetCard.tsx
@@ -1,14 +1,19 @@
 import { FC, useState } from 'react'
 
-import { HStack, Stack, useDisclosure } from '@chakra-ui/react'
+import {
+  HStack,
+  Stack,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react'
 import { useRecommendTweet } from '@wsvvrijheid/services'
 import { useAuthSelector } from '@wsvvrijheid/store'
 import { RecommendedTweetCreateInput } from '@wsvvrijheid/types'
 import { TimelineTweet as TimelineTweetType } from '@wsvvrijheid/types'
-import { FaInstagram, FaTwitter, FaWhatsapp } from 'react-icons/fa'
 
-import { CreateTweetForm, SocialButtons } from '../../components'
+import { CreateTweetForm } from '../../components'
 import { TimelineLocalTweet, TimelineTweet } from '../TimelineTweet'
+import { RecommendedSocialButtons } from './RecommendedSocialButtons'
 import { RecommendedTweetCardProps } from './types'
 
 export const RecommendedTweetCard: FC<RecommendedTweetCardProps> = ({
@@ -38,37 +43,41 @@ export const RecommendedTweetCard: FC<RecommendedTweetCardProps> = ({
     await mutateAsync(recommendedTweet)
     onClose()
   }
+  const isVertical = useBreakpointValue({
+    base: true,
+    lg: false,
+  })
 
-  const recomendedTweetButtons = [
-    {
-      label: 'Twitter',
-      icon: FaTwitter,
-      link: {
-        en: 'https://twitter.com/sanatduragi_nl',
-        tr: 'https://twitter.com/sanatduragi_nl',
-        nl: 'https://twitter.com/sanatduragi_nl',
-      },
-    },
-    {
-      label: 'WhatsApp',
-      icon: FaWhatsapp,
-      link: {
-        en: 'https://api.whatsapp.com/send?phone=31685221308',
-        tr: 'https://api.whatsapp.com/send?phone=31685221308',
-        nl: 'https://api.whatsapp.com/send?phone=31685221308',
-      },
-    },
-    {
-      label: 'Instagram',
-      icon: FaInstagram,
-      link: {
-        en: 'https://instagram.com/sanatduragi.nl',
-        tr: 'https://instagram.com/sanatduragi.nl',
-        nl: 'https://instagram.com/sanatduragi.nl',
-      },
-    },
-  ]
-  console.log('recommender in card', tweet?.recommender)
+  // const recomendedTweetButtons = [
+  //   {
+  //     label: 'Twitter',
+  //     icon: FaTwitter,
+  //     link: {
+  //       en: 'https://twitter.com/sanatduragi_nl',
+  //       tr: 'https://twitter.com/sanatduragi_nl',
+  //       nl: 'https://twitter.com/sanatduragi_nl',
+  //     },
+  //   },
+  //   {
+  //     label: 'WhatsApp',
+  //     icon: FaWhatsapp,
+  //     link: {
+  //       en: 'https://api.whatsapp.com/send?phone=31685221308',
+  //       tr: 'https://api.whatsapp.com/send?phone=31685221308',
+  //       nl: 'https://api.whatsapp.com/send?phone=31685221308',
+  //     },
+  //   },
+  //   {
+  //     label: 'Instagram',
+  //     icon: FaInstagram,
+  //     link: {
+  //       en: 'https://instagram.com/sanatduragi.nl',
+  //       tr: 'https://instagram.com/sanatduragi.nl',
+  //       nl: 'https://instagram.com/sanatduragi.nl',
+  //     },
+  //   },
+  // ]
+  console.log('recommender in card', tweet)
   const onEdit = (data: TimelineLocalTweet) => {
     setEditTweet(data.tweet)
     onOpen()
@@ -76,11 +85,13 @@ export const RecommendedTweetCard: FC<RecommendedTweetCardProps> = ({
   return (
     <Stack
       spacing={4}
-      align={'start'}
+      // align={'start'}
       bg={'white'}
       rounded={'md'}
       shadow={'sm'}
       p={4}
+      align={'space-between'}
+      overflow="hidden"
     >
       {editTweet && (
         <CreateTweetForm
@@ -100,7 +111,11 @@ export const RecommendedTweetCard: FC<RecommendedTweetCardProps> = ({
           key={key}
         />
       </HStack>
-      <SocialButtons items={recomendedTweetButtons} />
+      <RecommendedSocialButtons
+        tweet={tweet}
+        isVertical={isVertical}
+        //items={recomendedTweetButtons}
+      />
     </Stack>
   )
 }
