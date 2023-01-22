2b4d4e53fc3e57f9d378ce5b6db923b5d6d47909
MustafaTRKYLMZ <cana66331@gmail.com>
2023-01-22 12:13:56 +0100

    filter added

--- apps/admin/pages/posts/index.tsx
+++ apps/admin/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 
+import { MenuItemOption, MenuOptionGroup } from '@chakra-ui/react'
 import { useSearchModel } from '@wsvvrijheid/services'
 import { ApprovalStatus, Post, Sort, StrapiLocale } from '@wsvvrijheid/types'
 import {
@@ -23,6 +24,10 @@ const PostsPage = () => {
   const { locale, push } = useRouter()
 
   const [sort, setSort] = useState<Sort>()
+  const [posts, setPosts] = useState<Post[]>([])
+
+  const [sources, setSources] = useState<string[]>([])
+  const [filters, setFilter] = useState<string[]>([])
 
   const postsQuery = useSearchModel<Post>({
     url: 'api/posts',
@@ -39,17 +44,42 @@ const PostsPage = () => {
     search ? setSearchTerm(search) : setSearchTerm(undefined)
   }
 
-  useUpdateEffect(() => {
-    postsQuery.refetch()
-  }, [locale, searchTerm, sort, status])
-
-  const posts = postsQuery?.data?.data
+  const postsData = postsQuery?.data?.data
   const totalCount = postsQuery?.data?.meta?.pagination?.pageCount
 
-  const mappedPosts = posts?.map(post => ({
+  const mappedPosts = postsData?.map(post => ({
     ...post,
     translates: post.localizations?.map(l => l.locale),
   }))
+  useUpdateEffect(() => {
+    postsQuery.refetch()
+    setPosts(mappedPosts)
+  }, [locale, searchTerm, sort, status])
+
+  useEffect(() => {
+    const localeData = postsQuery?.data?.data?.filter(d => d.locale === locale)
+
+    const filteredData = localeData?.filter(d =>
+      filters.length > 0 ? filters.includes(d?.hashtag?.title) : true,
+    )
+    console.log('filtered data', filteredData)
+    setSources([...new Set(localeData?.map(d => d.hashtag.title))])
+    //  setPosts(searchTerm ? handleSearch(filteredData) : filteredData)
+  }, [filters, locale, handleSearch, searchTerm])
+
+  const filterMenu = (
+    <MenuOptionGroup
+      title="Hastags"
+      type="checkbox"
+      onChange={(value: string[]) => setFilter(value)}
+    >
+      {sources?.map(source => (
+        <MenuItemOption key={source} value={source}>
+          {source}
+        </MenuItemOption>
+      ))}
+    </MenuOptionGroup>
+  )
 
   const handleClick = (index: number, id: number) => {
     push(`/posts/${id}`)
@@ -59,6 +89,8 @@ const PostsPage = () => {
     <AdminLayout
       title={`${status} Posts`}
       headerProps={{
+        filterMenu,
+        filterMenuCloseOnSelect: false,
         onSearch: handleSearch,
         searchPlaceHolder: 'Search by title or content',
         defaultLocale,
@@ -75,7 +107,7 @@ const PostsPage = () => {
       </ModelCreateModal>
       <DataTable<Post>
         columns={postColumns}
-        data={mappedPosts}
+        data={posts}
         totalCount={totalCount}
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
