a856f9a13b02022c9c15143a8c066b97e81b510c
MustafaTRKYLMZ <cana66331@gmail.com>
2023-02-17 16:54:41 +0100

    some translate table added

--- apps/admin/pages/translates/arts.tsx
+++ apps/admin/pages/translates/arts.tsx
@@ -1,12 +1,83 @@
-import { Box } from '@chakra-ui/react'
-import { AdminLayout } from '@wsvvrijheid/ui'
+import { useEffect, useState } from 'react'
 
-const ArtsTranslatePage = () => {
+import { MenuItem } from '@chakra-ui/react'
+import { useSearchModel } from '@wsvvrijheid/services'
+import { ApprovalStatus, Art, Sort, StrapiLocale } from '@wsvvrijheid/types'
+import { AdminLayout, ArtsTable, PageHeader } from '@wsvvrijheid/ui'
+import { useRouter } from 'next/router'
+import { FaArrowDown, FaArrowUp } from 'react-icons/fa'
+import { useUpdateEffect } from 'react-use'
+
+const ArtsTranslatesPage = () => {
+  const { query } = useRouter()
+  const [currentPage, setCurrentPage] = useState<number>()
+  const [searchTerm, setSearchTerm] = useState<string>()
+
+  // Client side query params (?status=pending)
+  const status = query.status as ApprovalStatus
+
+  const [sort, setSort] = useState<Sort>()
+
+  const { locale } = useRouter()
+
+  const artsQuery = useSearchModel<Art>({
+    url: 'api/arts',
+    populate: [
+      'artist.user.avatar',
+      'categories',
+      'image',
+      'likers',
+      'localizations',
+    ],
+    page: currentPage || 1,
+    pageSize: 10,
+    searchTerm,
+    sort,
+    locale: locale as StrapiLocale,
+    statuses: ['pending'],
+  })
+
+  useEffect(() => setCurrentPage(1), [status])
+  const handleSearch = (search: string) => {
+    search ? setSearchTerm(search) : setSearchTerm(undefined)
+  }
+
+  useUpdateEffect(() => {
+    artsQuery.refetch()
+  }, [locale, searchTerm, sort, status])
+
+  const arts = artsQuery?.data?.data
+  const totalCount = artsQuery?.data?.meta?.pagination?.pageCount
+
+  const mappedArts = arts?.map(art => ({
+    ...art,
+    translates: art.localizations?.map(l => l.locale),
+  }))
+
   return (
-    <AdminLayout title="ArtsTranslatePage">
-      <Box>ArtsTranslatePage</Box>
+    <AdminLayout title={`Translated Arts`}>
+      <PageHeader
+        onSearch={handleSearch}
+        searchPlaceHolder={'Search arts by title or artist'}
+        sortMenu={[
+          <MenuItem key="asc" icon={<FaArrowUp />}>
+            Name Asc
+          </MenuItem>,
+          <MenuItem key="desc" icon={<FaArrowDown />}>
+            Name Desc
+          </MenuItem>,
+        ]}
+      />
+      <ArtsTable
+        data={mappedArts}
+        onSuccess={artsQuery.refetch}
+        totalCount={totalCount}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        onSort={setSort}
+      />
     </AdminLayout>
   )
 }
 
-export default ArtsTranslatePage
+export default ArtsTranslatesPage
---
+++ libs/ui/src/admin/TranslatedData/types.tsx
@@ -0,0 +1,5 @@
+export type TranslatedFieldProps = {
+  url: string
+  model: ''
+  fields: ''
+}
