91a118d116a9a2272a424390a6d184bdfcc311af
7alip <talipaltas@gmail.com>
2023-02-17 06:24:14 +0100

    Update vercel url

--- apps/foundation/pages/api/payment/index.ts
+++ apps/foundation/pages/api/payment/index.ts
@@ -1,4 +1,4 @@
-import { TOKEN, VERCEL_URL } from '@wsvvrijheid/config'
+import { TOKEN, SITE_URL } from '@wsvvrijheid/config'
 import { Mutation } from '@wsvvrijheid/lib'
 import { mollieClient } from '@wsvvrijheid/mollie'
 import { Donate, DonateCreateInput } from '@wsvvrijheid/types'
@@ -30,8 +30,8 @@ export default async function handler(req, res) {
       id: donate.id,
     },
     description: description ?? 'no description',
-    redirectUrl: `${VERCEL_URL}/donate/complete?id=${donate.id}`,
-    webhookUrl: `${VERCEL_URL}/api/payment/status`,
+    redirectUrl: `${SITE_URL}/donate/complete?id=${donate.id}`,
+    webhookUrl: `${SITE_URL}/api/payment/status`,
   })
 
   await res.status(200).send(payment.getCheckoutUrl())
--- libs/config/src/consts.ts
+++ libs/config/src/consts.ts
@@ -1,7 +1,12 @@
+export const VERCEL_URL = process.env['VERCEL_URL']
 export const SITE_URL =
   typeof window !== 'undefined'
     ? window.location.origin
-    : process.env['NX_VERCEL_URL'] || ''
+    : VERCEL_URL?.startsWith('localhost')
+    ? `http://${VERCEL_URL}`
+    : VERCEL_URL?.startsWith('http')
+    ? VERCEL_URL
+    : `https://${VERCEL_URL}`
 
 export const API_URL = process.env['NX_API_URL']
 export const TOKEN = process.env['NX_API_TOKEN']
@@ -11,7 +16,5 @@ export const COOKIE_PASSWORD = process.env['NX_SECRET_COOKIE_PASSWORD']
 export const DEEPL_API_KEY = process.env['NX_DEEPL_API_KEY']
 export const GA_MEASUREMENT_ID = process.env['NX_GA_MEASUREMENT_ID']
 export const IS_PROD = process.env['NODE_ENV'] === 'production'
-export const VERCEL_URL =
-  process.env['NX_VERCEL_URL'] || process.env['VERCEL_URL']
 export const DONATION_ENABLED = process.env['NX_DONATION_ENABLED'] === 'true'
 export const MOLLIE_KEY = process.env['NX_MOLLIE_KEY']
