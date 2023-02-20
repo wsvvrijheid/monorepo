17d3a3465fb1107db23ba476ba7b57f011c8542e
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-02-20 19:38:17 +0100

    Refactor hashtags

--- apps/samenvvv/pages/index.tsx
+++ apps/samenvvv/pages/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 
 import { Box, Button, Heading, Stack, Text } from '@chakra-ui/react'
 import { searchModel } from '@wsvvrijheid/services'
-import { Hashtag, StrapiLocale, StrapiMeta } from '@wsvvrijheid/types'
+import { Hashtag, StrapiLocale } from '@wsvvrijheid/types'
 import { Navigate, Container } from '@wsvvrijheid/ui'
 import { getItemLink } from '@wsvvrijheid/utils'
 import { GetStaticProps } from 'next'
@@ -14,19 +14,13 @@ import { Layout } from '../components'
 import { HashtagsSummary } from '../components/HashtagsSummary/HashtagsSummary'
 import i18nConfig from '../next-i18next.config'
 
-export type SearchedHashtags = {
-  data: Hashtag[]
-  meta: StrapiMeta
-}
-
 interface HomeProps {
   seo: NextSeoProps
   link: string
-  hashtags: SearchedHashtags
-  links: string[]
+  hashtags: Hashtag[]
 }
 
-const Home: FC<HomeProps> = ({ seo, link, hashtags, links }) => {
+const Home: FC<HomeProps> = ({ seo, link, hashtags }) => {
   const { t } = useTranslation()
 
   return (
@@ -86,14 +80,14 @@ export const getStaticProps: GetStaticProps = async context => {
     tr: 'Anasayfa',
   }
 
-  const hashtags = await searchModel<Hashtag>({
+  const { data: hashtags } = await searchModel<Hashtag>({
     url: 'api/hashtags',
     locale,
     statuses: ['approved'],
     pageSize: 4,
   })
 
-  const latestHashtag = hashtags?.data?.[0] || null
+  const latestHashtag = hashtags?.[0] || null
 
   const link = getItemLink(latestHashtag, locale, 'hashtag')
 
