cbd40f3c9289439ecf9da7d80287ed51ff9bb48c
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-02-27 15:12:24 +0100

    Add manifest and wrap Head with PWAHead

--- apps/samenvvv/pages/_document.tsx
+++ apps/samenvvv/pages/_document.tsx
@@ -1,61 +1,64 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import { GA_MEASUREMENT_ID } from '@wsvvrijheid/config'
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
+import { PWAHead } from 'next-pwa'
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head>
-          <link
-            rel="icon"
-            type="image/x-icon"
-            href="https://api/wsvvrijheid.nl/uploads/favicon_1ceb7b6658.ico"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="https://api/wsvvrijheid.nl/uploads/apple_touch_icon_f670623b64.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="https://api/wsvvrijheid.nl/uploads/favicon_32x32_995fe02a98.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="https://api/wsvvrijheid.nl/uploads/favicon_16x16_257e6b0697.png"
-          />
-          <link
-            rel="manifest"
-            href="https://api/wsvvrijheid.nl/uploads/site_6c805a152a.webmanifest"
-          />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;900&display=swap"
-            rel="stylesheet"
-          />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}', {
-                page_path: window.location.pathname,
-              });
-          `,
-            }}
-          />
-        </Head>
+        <PWAHead>
+          <Head>
+            <link
+              rel="icon"
+              type="image/x-icon"
+              href="https://api/wsvvrijheid.nl/uploads/favicon_1ceb7b6658.ico"
+            />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="https://api/wsvvrijheid.nl/uploads/apple_touch_icon_f670623b64.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="https://api/wsvvrijheid.nl/uploads/favicon_32x32_995fe02a98.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="https://api/wsvvrijheid.nl/uploads/favicon_16x16_257e6b0697.png"
+            />
+            <link
+              rel="manifest"
+              href="https://api/wsvvrijheid.nl/uploads/site_6c805a152a.webmanifest"
+            />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;900&display=swap"
+              rel="stylesheet"
+            />
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+            `,
+              }}
+            />
+          </Head>
+        </PWAHead>
         <body>
           <ColorModeScript initialColorMode="light" />
           <Main />
