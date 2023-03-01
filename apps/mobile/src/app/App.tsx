2d570ecda720bd874dd466f1f098793093048f47
7alip <talipaltas@gmail.com>
2023-03-02 00:34:52 +0100

    Add react native project

---
+++ apps/mobile/src/app/App.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, { useRef } from 'react'
+
+import { store } from '@wsvvrijheid/store'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { Provider } from 'react-redux'
+
+import Content from './Content'
+
+// TODO: Add tanstack query https://codesandbox.io/s/github/tanstack/query/tree/main/examples/react/react-native?from-embed
+export const App = () => {
+  const scrollViewRef = useRef<null | ScrollView>(null)
+
+  return (
+    <Provider store={store}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          ref={ref => {
+            scrollViewRef.current = ref
+          }}
+          contentInsetAdjustmentBehavior="automatic"
+        >
+          <View>
+            <TouchableOpacity>
+              <View>
+                <Text>Post Maker</Text>
+                <Content />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Provider>
+  )
+}
+
+export default App
