38e0b43e80fdf7733676e3922c700fbf6ab5c2d3
7alip <talipaltas@gmail.com>
2023-03-03 09:34:55 +0100

    Update kunsthalte font

--- libs/config/src/theme/fonts.ts
+++ libs/config/src/theme/fonts.ts
@@ -1,13 +1,17 @@
-import { Poppins, Lemonada } from '@next/font/google'
+import { Poppins, MuseoModerno } from '@next/font/google'
 
 const poppins = Poppins({
   weight: ['400', '600', '700', '900'],
   subsets: ['latin'],
 })
-const lemonada = Lemonada({ subsets: ['latin'] })
+
+const museo = MuseoModerno({
+  weight: ['400', '600', '700', '900'],
+  subsets: ['latin'],
+})
 
 export const fonts = {
   body: poppins.style.fontFamily,
   heading: poppins.style.fontFamily,
-  club: lemonada.style.fontFamily,
+  club: museo.style.fontFamily,
 }
