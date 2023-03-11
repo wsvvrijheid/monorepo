6d03477b24e2c0ae95258e78b0093e62ffbb0c39
7alip <talipaltas@gmail.com>
2023-03-11 14:21:53 +0100

    Make min donation 5 euros

--- libs/config/src/consts.ts
+++ libs/config/src/consts.ts
@@ -1,10 +1,11 @@
-export const VERCEL_URL = process.env['VERCEL_URL']
+export const VERCEL_URL = `https://${process.env['VERCEL_URL']}`
+const BASE_URL = process.env['NX_SITE_URL']
+const DEV_URL = 'http://localhost:4200'
+const PROD_URL = BASE_URL || VERCEL_URL
+const CLIENT_URL = typeof window !== 'undefined' && window.location.origin
+
 export const SITE_URL =
-  typeof window !== 'undefined'
-    ? window.location.origin
-    : process.env['NODE_ENV'] === 'development'
-    ? process.env['NX_SITE_URL'] || 'http://localhost:4200'
-    : `https://${VERCEL_URL}`
+  CLIENT_URL || process.env['NODE_ENV'] === 'development' ? DEV_URL : PROD_URL
 
 export const API_URL = process.env['NX_API_URL']
 export const TOKEN = process.env['NX_API_TOKEN']
