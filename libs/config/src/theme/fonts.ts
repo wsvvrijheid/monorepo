98c1cd5ff7637fed298e9a8631b7e3e41adf1a4b
7alip <talipaltas@gmail.com>
2023-03-19 12:12:26 +0100

    Update announcement design

--- libs/config/src/theme/fonts.ts
+++ libs/config/src/theme/fonts.ts
@@ -3,11 +3,13 @@ import { Poppins, MuseoModerno } from '@next/font/google'
 const poppins = Poppins({
   weight: ['400', '600', '700', '900'],
   subsets: ['latin'],
+  display: 'swap',
 })
 
 const museo = MuseoModerno({
   weight: ['400', '600', '700', '900'],
   subsets: ['latin'],
+  display: 'swap',
 })
 
 export const fonts = {
