7895a8c5e6ab3b4bedd2e446890f991ded1cc167
7alip <talipaltas@gmail.com>
2023-04-02 15:05:40 +0200

    Fix site url

--- libs/config/src/consts.ts
+++ libs/config/src/consts.ts
@@ -1,11 +1,5 @@
-export const VERCEL_URL = `https://${process.env['VERCEL_URL']}`
-const BASE_URL = process.env['NX_SITE_URL']
-const DEV_URL = 'http://localhost:4200'
-const PROD_URL = BASE_URL || VERCEL_URL
-const CLIENT_URL = typeof window !== 'undefined' && window.location.origin
-
 export const SITE_URL =
-  CLIENT_URL || process.env['NODE_ENV'] === 'development' ? DEV_URL : PROD_URL
+  process.env['NX_SITE_URL'] || `https://${process.env['VERCEL_URL']}`
 
 export const API_URL = process.env['NX_API_URL']
 export const TOKEN = process.env['NX_API_TOKEN']
