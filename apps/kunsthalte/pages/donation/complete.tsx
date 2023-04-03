2ba0b7edd3e25a4bc74988154cf33cd070c18e21
7alip <talipaltas@gmail.com>
2023-04-03 12:06:18 +0200

    Add donation templates

--- apps/kunsthalte/pages/donation/complete.tsx
+++ apps/kunsthalte/pages/donation/complete.tsx
@@ -1,94 +1,30 @@
 import { FC } from 'react'
 
-import { Center } from '@chakra-ui/react'
-import { InferGetServerSidePropsType } from 'next'
+import { PaymentStatus } from '@mollie/api-client'
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { TOKEN } from '@wsvvrijheid/config'
 import { Mutation, Request } from '@wsvvrijheid/lib'
 import { mollieClient } from '@wsvvrijheid/mollie'
 import { Donation, StrapiUrl } from '@wsvvrijheid/types'
-import { Container, DonationResultAlert } from '@wsvvrijheid/ui'
-
-import { Layout } from '../../components'
+import { AdminLayout, DonationCompleteTemplate } from '@wsvvrijheid/ui'
 
 type DonationCompletePageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
 >
 
-// TODO Make transaction detail messages more user friendly and create translations
-// TODO Add translations
 const DonationComplete: FC<DonationCompletePageProps> = ({ status }) => {
-  const renderStatus = () => {
-    if (status === 'paid') {
-      return (
-        <DonationResultAlert
-          title="Thank you"
-          description="We received your donation"
-          status="success"
-        />
-      )
-    }
-
-    if (status === 'open') {
-      return (
-        <DonationResultAlert
-          status="warning"
-          title="Payment not completed!"
-          description="It seems your payment has not been competed"
-        />
-      )
-    }
-
-    if (status === 'expired') {
-      return (
-        <DonationResultAlert
-          status="warning"
-          title="Expired"
-          description="Payment link has been expired"
-        />
-      )
-    }
-
-    if (status === 'canceled') {
-      return (
-        <DonationResultAlert
-          status="info"
-          title="Cancelled"
-          description="You canceled the donation"
-        />
-      )
-    }
-
-    if (status === 'failed') {
-      return (
-        <DonationResultAlert
-          status="error"
-          title="Failed"
-          description="Transaction failed"
-        />
-      )
-    }
-
-    return (
-      <DonationResultAlert
-        status="warning"
-        title="Not found"
-        description="Transaction could not be found. Please contact!"
-      />
-    )
-  }
-
   return (
-    <Layout seo={{ title: 'Payment' }}>
-      <Container maxWidth="container.sm">
-        <Center minH="70vh">{renderStatus()}</Center>
-      </Container>
-    </Layout>
+    <AdminLayout seo={{ title: 'Payment' }}>
+      <DonationCompleteTemplate status={status} />
+    </AdminLayout>
   )
 }
 
-export const getServerSideProps = async context => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
   const { query } = context
 
   const response = await Request.single<Donation>({
@@ -103,7 +39,7 @@ export const getServerSideProps = async context => {
 
   const status = payment?.status || null
 
-  if (status === 'paid') {
+  if (status === PaymentStatus.paid) {
     await Mutation.post(`api/donates/email/${query.id}` as StrapiUrl, {}, TOKEN)
   }
 
