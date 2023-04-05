fd0145be263e25292d8f0e71b08c562cc5682a0b
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-04-05 18:12:22 +0200

    add CourseApplication & add fixes

---
+++ apps/api/src/api/course-application/services/course-application.ts
@@ -0,0 +1,9 @@
+/**
+ * course-application service
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreService(
+  'api::course-application.course-application',
+)
