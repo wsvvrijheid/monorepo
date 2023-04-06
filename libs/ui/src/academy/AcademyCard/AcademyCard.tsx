547f64f0c2827109904531b82568417d1243f40d
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-04-06 20:23:45 +0200

    implement AcademyCard

--- libs/ui/src/academy/AcademyCard/AcademyCard.tsx
+++ libs/ui/src/academy/AcademyCard/AcademyCard.tsx
@@ -1,20 +1,30 @@
 import { FC } from 'react'
 
-import { Card, CardBody, Text } from '@chakra-ui/react'
+import { Card, CardBody, Link, Text } from '@chakra-ui/react'
 
-// import { WImage } from '../../components'
+import { WImage } from '../../components'
 
 type AcademyCardProps = {
   children: React.ReactNode
-  // image: string
+  href: string
+  image: string
 }
 
-export const AcademyCard: FC<AcademyCardProps> = ({ children }) => {
+export const AcademyCard: FC<AcademyCardProps> = ({
+  children,
+  href,
+  image,
+}) => {
   return (
-    <Card w={64} h={64}>
-      <CardBody>
-        <Text>{children}</Text>
-      </CardBody>
-    </Card>
+    <Link href={href}>
+      <Card w={64} h={64}>
+        <CardBody position="relative">
+          <WImage src={image} />
+          <Text position="absolute" bottom="5">
+            {children}
+          </Text>
+        </CardBody>
+      </Card>
+    </Link>
   )
 }
