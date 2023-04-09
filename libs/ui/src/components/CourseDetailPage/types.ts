cec147df7ff3ac098415b6316b01d9b5dc857da3
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2023-04-09 14:39:03 +0200

    commit befor dev merge

--- libs/ui/src/components/CourseDetailPage/types.ts
+++ libs/ui/src/components/CourseDetailPage/types.ts
@@ -1,9 +1,13 @@
 export type CourseDetailPageProps = {
-  image: string
   title: string
   description: string
   content?: string
+  location: string
+  instructor?: string
   price: number
+  quota?: number
+  image: string
   startDate: string
-  dueDate: string
+  endDate: string
+  faqs: { question: string; answer: string }[]
 }
