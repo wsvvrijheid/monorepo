b9314a2d1dc510f25853244575dd56ebef8ee1df
7alip <talipaltas@gmail.com>
2023-04-09 05:55:30 +0200

    Add course detail page

---
+++ apps/admin/pages/courses/[id].tsx
@@ -0,0 +1,77 @@
+import { FC } from 'react'
+
+import { Box } from '@chakra-ui/react'
+import { InferGetServerSidePropsType } from 'next'
+// import { useRouter } from 'next/router'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { NextSeoProps } from 'next-seo'
+
+// import { useModelById } from '@wsvvrijheid/services'
+// import { Course } from '@wsvvrijheid/types'
+import { AdminLayout } from '@wsvvrijheid/ui'
+
+import i18nConfig from '../../next-i18next.config'
+
+type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>
+
+const CoursePage: FC<PageProps> = ({ seo }) => {
+  // const router = useRouter()
+  // const { query } = router
+
+  // const id = Number(query.id as string)
+  // const {
+  //   data: course,
+  //   isLoading,
+  //   refetch,
+  // } = useModelById<Course>({
+  //   url: 'api/courses',
+  //   id,
+  // })
+
+  return (
+    <AdminLayout
+      seo={seo}
+      // isLoading={isLoading}
+      hasBackButton
+    >
+      <Box p={6} rounded="md" bg="white" shadow="md">
+        {/* {course && (
+          <ModelEditForm<Course>
+            url="api/hashtags"
+            model={course}
+            schema={courseSchema}
+            fields={courseFields}
+            onSuccess={refetch}
+          />
+        )} */}
+      </Box>
+    </AdminLayout>
+  )
+}
+
+export const getServerSideProps = async context => {
+  const { locale } = context
+
+  const title = {
+    en: 'Course',
+    tr: 'Kurs',
+    nl: 'Course',
+  }
+
+  const seo: NextSeoProps = {
+    title: title[locale],
+  }
+
+  return {
+    props: {
+      seo,
+      ...(await serverSideTranslations(
+        locale,
+        ['common', 'admin'],
+        i18nConfig,
+      )),
+    },
+  }
+}
+
+export default CoursePage
