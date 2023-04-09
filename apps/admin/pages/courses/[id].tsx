0cfc85be0735e0bb0efc91d549c012239a5f84ce
7alip <talipaltas@gmail.com>
2023-04-09 06:34:33 +0200

    Add course detail page

--- apps/admin/pages/courses/[id].tsx
+++ apps/admin/pages/courses/[id].tsx
@@ -2,40 +2,41 @@ import { FC } from 'react'
 
 import { Box } from '@chakra-ui/react'
 import { InferGetServerSidePropsType } from 'next'
-// import { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeoProps } from 'next-seo'
 
-// import { useModelById } from '@wsvvrijheid/services'
-// import { Course } from '@wsvvrijheid/types'
-import { AdminLayout } from '@wsvvrijheid/ui'
+import { useModelById } from '@wsvvrijheid/services'
+import { Course } from '@wsvvrijheid/types'
+import {
+  AdminLayout,
+  courseFields,
+  courseSchema,
+  ModelEditForm,
+} from '@wsvvrijheid/ui'
 
 import i18nConfig from '../../next-i18next.config'
 
 type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>
 
 const CoursePage: FC<PageProps> = ({ seo }) => {
-  // const router = useRouter()
-  // const { query } = router
+  const router = useRouter()
+  const { query } = router
 
-  // const id = Number(query.id as string)
-  // const {
-  //   data: course,
-  //   isLoading,
-  //   refetch,
-  // } = useModelById<Course>({
-  //   url: 'api/courses',
-  //   id,
-  // })
+  const id = Number(query.id as string)
+  const {
+    data: course,
+    isLoading,
+    refetch,
+  } = useModelById<Course>({
+    url: 'api/courses',
+    id,
+  })
 
   return (
-    <AdminLayout
-      seo={seo}
-      // isLoading={isLoading}
-      hasBackButton
-    >
+    <AdminLayout seo={seo} isLoading={isLoading} hasBackButton>
       <Box p={6} rounded="md" bg="white" shadow="md">
-        {/* {course && (
+        {course && (
           <ModelEditForm<Course>
             url="api/hashtags"
             model={course}
@@ -43,7 +44,7 @@ const CoursePage: FC<PageProps> = ({ seo }) => {
             fields={courseFields}
             onSuccess={refetch}
           />
-        )} */}
+        )}
       </Box>
     </AdminLayout>
   )
