86f6e611656927d90b2698bd8a0e1b0d604fab9a
7alip <talipaltas@gmail.com>
2023-04-10 09:35:38 +0200

    Move courses page

--- libs/config/src/data/index.ts
+++ libs/config/src/data/index.ts
@@ -1,6 +1,7 @@
 import ABOUT_US from './about-us.json'
 import ABOUT_DATA from './abouts.json'
 import COUNTRIES from './countries.json'
+import COURSES from './courses.json'
 import DEFAULT_POST_SENTENCES from './defaultPostSentences.json'
 import PLATFORMS from './platforms.json'
 import ROUTES from './routes.json'
@@ -9,6 +10,7 @@ export {
   ABOUT_DATA,
   ABOUT_US,
   COUNTRIES,
+  COURSES,
   DEFAULT_POST_SENTENCES,
   PLATFORMS,
   ROUTES,
