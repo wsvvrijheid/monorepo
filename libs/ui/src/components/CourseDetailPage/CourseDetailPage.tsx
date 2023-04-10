eba68bcd4b951b2cc7ab3ea0947b5ba99d8cb28f
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-04-10 17:50:53 +0200

    Update CourseDetailPage.tsx

--- libs/ui/src/components/CourseDetailPage/CourseDetailPage.tsx
+++ libs/ui/src/components/CourseDetailPage/CourseDetailPage.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 
-import { API_URL } from '@wsvvrijheid/config'
+import { API_URL, SITE_URL } from '@wsvvrijheid/config'
 import { StrapiLocale } from '@wsvvrijheid/types'
 
 import { CourseApplicationForm } from './CourseApplicationForm'
