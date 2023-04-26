c99b57531064689dd26ecd6388b8a33be4e61fd3
7alip <talipaltas@gmail.com>
2023-04-26 10:44:37 +0200

    Fix theme overflow settings

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react'
 
-import { ChakraProvider, createStandaloneToast } from '@chakra-ui/react'
+import {
+  ChakraProvider,
+  createStandaloneToast,
+  extendTheme,
+} from '@chakra-ui/react'
 import {
   Hydrate,
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { merge } from 'lodash'
 import { useRouter } from 'next/router'
 import { appWithTranslation } from 'next-i18next'
 import { DefaultSeo } from 'next-seo'
@@ -23,8 +28,22 @@ import '@uppy/core/dist/style.css'
 import '@uppy/dashboard/dist/style.css'
 import '@uppy/image-editor/dist/style.css'
 import '@uppy/url/dist/style.css'
-import 'react-medium-image-zoom/dist/styles.css'
 import 'react-markdown-editor-lite/lib/index.css'
+import 'react-medium-image-zoom/dist/styles.css'
+
+const theme = merge(themes.admin, {
+  styles: {
+    global: {
+      body: {
+        overflow: 'hidden',
+      },
+      '#__next': {
+        h: '100vh',
+        overflow: 'hidden',
+      },
+    },
+  },
+})
 
 const { ToastContainer } = createStandaloneToast()
 
@@ -48,7 +67,7 @@ function MyApp({ Component, pageProps }) {
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <ReduxProvider store={store}>
-          <ChakraProvider theme={themes.admin}>
+          <ChakraProvider theme={extendTheme(theme)}>
             <DefaultSeo {...defaultSeo.admin[locale]} />
             <Component {...pageProps} />
             <ToastContainer />
