8e31c2c37c416dd6734cff41bb4015a0a96e9d32
MSKose <saitmustafa1998@gmail.com>
2023-05-11 20:31:22 +0200

    cron jobs added

--- apps/api/config/env/production/cron-tasks/userStats.ts
+++ apps/api/config/env/production/cron-tasks/userStats.ts
@@ -1,12 +1,14 @@
 import { getStats } from '../../../../src/libs'
 
 export default async ({ strapi }) => {
-  let userStatArr = [];
-  const users = await strapi.plugins['users-permissions'].services.user.fetchAll();
+  let userStatArr = []
+  const users = await strapi.plugins[
+    'users-permissions'
+  ].services.user.fetchAll()
 
   for (const user of users) {
-    userStatArr.push(await getStats(user.id));
+    userStatArr.push(await getStats(user.id))
   }
 
-  return userStatArr;
+  return userStatArr
 }
