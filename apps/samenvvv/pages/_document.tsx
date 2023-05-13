a5171abb275c9c649766f117708ec715afef19cc
7alip <talipaltas@gmail.com>
2023-05-13 09:54:40 +0200

    Revert local images

--- apps/samenvvv/pages/_document.tsx
+++ apps/samenvvv/pages/_document.tsx
@@ -8,25 +8,32 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/x-icon"
+            href="https://api.wsvvrijheid.nl/uploads/favicon_1ceb7b6658.ico"
+          />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="https://api.wsvvrijheid.nl/uploads/apple_touch_icon_f670623b64.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="https://api.wsvvrijheid.nl/uploads/favicon_32x32_995fe02a98.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="https://api.wsvvrijheid.nl/uploads/favicon_16x16_257e6b0697.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            rel="manifest"
+            href="https://api.wsvvrijheid.nl/uploads/site_6c805a152a.webmanifest"
+          />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
