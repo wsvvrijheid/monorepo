44a5c379a1119d04a2271ff49f42676d2ecc232e
MSKose <saitmustafa1998@gmail.com>
2023-05-13 20:26:16 +0200

    $between added

--- apps/api/config/env/production/cron-tasks/userStats.ts
+++ apps/api/config/env/production/cron-tasks/userStats.ts
@@ -2,7 +2,9 @@ import { getStats } from '../../../../src/libs'
 
 export default async ({ strapi }) => {
   let userStatArr = []
-  const users = await strapi.plugins['users-permissions'].services.user.fetchAll()
+  const users = await strapi.plugins[
+    'users-permissions'
+  ].services.user.fetchAll()
 
   for (const user of users) {
     userStatArr.push(await getStats(user.id))
