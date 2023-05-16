727140149c2b4ec171895effa670cc817eea5bad
7alip <talipaltas@gmail.com>
2023-05-16 09:44:36 +0200

    Fix redis key

--- apps/admin/pages/api/kv/posts.ts
+++ apps/admin/pages/api/kv/posts.ts
@@ -12,7 +12,7 @@ const handler = async (req: NextRequest) => {
     if (method === 'POST') {
       const { id, value } = await req.json()
 
-      const response = await kv.rpush(`post-${id}`, value)
+      const response = await kv.rpush(`post_${id}`, value)
 
       return NextResponse.json(response)
     }
@@ -20,7 +20,7 @@ const handler = async (req: NextRequest) => {
     if (method === 'PUT') {
       const { id, value, index } = await req.json()
 
-      const response = await kv.lset(`post-${id}`, index, value)
+      const response = await kv.lset(`post_${id}`, index, value)
 
       return NextResponse.json(response)
     }
@@ -29,14 +29,14 @@ const handler = async (req: NextRequest) => {
       const id = req.nextUrl.searchParams.get('id').toString()
       const value = req.nextUrl.searchParams.get('value').toString()
 
-      const result = await kv.lrem(`post-${id}`, 0, value)
+      const result = await kv.lrem(`post_${id}`, 0, value)
 
       return NextResponse.json(result)
     }
 
     const id = req.nextUrl.searchParams.get('id').toString()
 
-    const result = await kv.lrange(`post-${id}`, 0, -1)
+    const result = await kv.lrange(`post_${id}`, 0, -1)
 
     return NextResponse.json(result)
   } catch (error) {
