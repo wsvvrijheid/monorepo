a4d7b0fb86e6f33667150873f28b20afef982fd7
7alip <talipaltas@gmail.com>
2023-05-16 09:14:52 +0200

    Add kv example to samen

---
+++ apps/samenvvv/pages/api/kv/posts.ts
@@ -0,0 +1,47 @@
+import kv from '@vercel/kv'
+import { NextRequest, NextResponse } from 'next/server'
+
+export const config = {
+  runtime: 'edge',
+}
+
+const handler = async (req: NextRequest) => {
+  const method = req.method
+
+  try {
+    if (method === 'POST') {
+      const { id, value } = await req.json()
+
+      const response = await kv.rpush(`post-${id}`, value)
+
+      return NextResponse.json(response)
+    }
+
+    if (method === 'PUT') {
+      const { id, value, index } = await req.json()
+
+      const response = await kv.lset(`post-${id}`, index, value)
+
+      return NextResponse.json(response)
+    }
+
+    if (method === 'DELETE') {
+      const id = req.nextUrl.searchParams.get('id').toString()
+      const value = req.nextUrl.searchParams.get('value').toString()
+
+      const result = await kv.lrem(`post-${id}`, 0, value)
+
+      return NextResponse.json(result)
+    }
+
+    const id = req.nextUrl.searchParams.get('id').toString()
+
+    const result = await kv.lrange(`post-${id}`, 0, -1)
+
+    return NextResponse.json(result)
+  } catch (error) {
+    return NextResponse.error()
+  }
+}
+
+export default handler
