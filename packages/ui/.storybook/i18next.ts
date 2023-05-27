a4ad9ed75cf33d097fddf57a0c060f9f56ba7592
7alip <talipaltas@gmail.com>
2023-05-27 17:32:28 +0200

    Add storybook

---
+++ packages/ui/.storybook/i18next.ts
@@ -0,0 +1,15 @@
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+    defaultNS: 'common',
+  })
+
+export default i18n
---
+++ packages/ui/.storybook/main.ts
@@ -0,0 +1,42 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+import path from 'path'
+
+const config: StorybookConfig = {
+  stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@chakra-ui/storybook-addon'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],
+  webpackFinal: async config => {
+    if (config.resolve) {
+      config.resolve.fallback = {
+        fs: require.resolve('browserify-fs'),
+        path: require.resolve('path-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        stream: require.resolve('stream-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+      }
+
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@wsvvrijheid/config': path.resolve(__dirname, '../../config'),
+        '@wsvvrijheid/context': path.resolve(__dirname, '../../context'),
+        '@wsvvrijheid/lib': path.resolve(__dirname, '../../lib'),
+        '@wsvvrijheid/mocks': path.resolve(__dirname, '../../mocks'),
+        '@wsvvrijheid/mollie': path.resolve(__dirname, '../../mollie'),
+        '@wsvvrijheid/secrets': path.resolve(__dirname, '../../secrets'),
+        '@wsvvrijheid/services': path.resolve(__dirname, '../../services'),
+        '@wsvvrijheid/types': path.resolve(__dirname, '../../types'),
+        '@wsvvrijheid/ui': path.resolve(__dirname, '../../ui'),
+        '@wsvvrijheid/utils': path.resolve(__dirname, '../../utils'),
+        'next-i18next': 'react-i18next',
+      }
+    }
+
+    return config
+  },
+}
+
+export default config
