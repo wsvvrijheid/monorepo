ab50460bfa4bb4c1df71c06f68f778c7859e3168
7alip <talipaltas@gmail.com>
2023-05-28 02:48:00 +0200

    Set pwa register

--- apps/foundation/next.config.js
+++ apps/foundation/next.config.js
@@ -1,36 +1,36 @@
 // @ts-check
-const withPWA = require("next-pwa");
-const { i18n } = require("./next-i18next.config");
+const withPWA = require('next-pwa')
 
- 
+const { i18n } = require('./next-i18next.config')
+
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["ui"],
+  transpilePackages: ['ui'],
   i18n,
   images: {
     deviceSizes: [320, 480, 720, 1080],
     imageSizes: [150],
     domains: [
-      "api.samenvvv.nl",
-      "api.wsvvrijheid.nl",
-      "pbs.twimg.com",
-      "samenvvv.nl",
-      "localhost",
+      'api.samenvvv.nl',
+      'api.wsvvrijheid.nl',
+      'pbs.twimg.com',
+      'samenvvv.nl',
+      'localhost',
     ],
   },
   modularizeImports: {
-    "date-fns": { transform: "date-fns/{{member}}" },
-    lodash: { transform: "lodash/{{member}}" },
+    'date-fns': { transform: 'date-fns/{{member}}' },
+    lodash: { transform: 'lodash/{{member}}' },
   },
-  outputFileTracing: true, 
-};
+  outputFileTracing: true,
+}
 
 module.exports = withPWA({
-  dest: "public",
+  dest: 'public',
+  register: true,
   // eslint-disable-next-line turbo/no-undeclared-env-vars
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig);
-
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig)
