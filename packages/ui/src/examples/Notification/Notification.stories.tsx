42f366e7168bef75c58ee355942ab8f8868a4775
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-05-30 21:53:44 +0200

    migrate to storybook 7

--- packages/ui/src/examples/Notification/Notification.stories.tsx
+++ packages/ui/src/examples/Notification/Notification.stories.tsx
@@ -1,5 +1,5 @@
 import { Container } from '@chakra-ui/react'
-import { Meta, Story } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 
 import { Notification } from './Notification'
 import { NotificationProps } from './types'
@@ -10,20 +10,22 @@ export default {
   decorators: [Story => <Container maxW="container.sm">{Story()}</Container>],
 } as Meta<typeof Notification>
 
-const Template: Story<NotificationProps> = args => <Notification {...args} />
+type Story = StoryObj<NotificationProps>
 
-export const Default = Template.bind({})
-Default.args = {
-  status: 'success',
-  title: 'Success',
-  description: 'Message has ben sent successfully',
-  isOpen: true,
+export const Default: Story = {
+  args: {
+    status: 'success',
+    title: 'Success',
+    description: 'Message has ben sent successfully',
+    isOpen: true,
+  },
 }
 
-export const Error = Template.bind({})
-Error.args = {
-  status: 'error',
-  title: 'Error',
-  description: 'An error occured while sending your message',
-  isOpen: true,
+export const Error: Story = {
+  args: {
+    status: 'error',
+    title: 'Error',
+    description: 'An error occured while sending your message',
+    isOpen: true,
+  },
 }
--- packages/ui/src/examples/StackExample/StackExample.stories.tsx
+++ packages/ui/src/examples/StackExample/StackExample.stories.tsx
@@ -1,10 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+
 import { StackExample } from './StackExample'
 
 export default {
   component: StackExample,
   title: 'Example/StackExample',
-}
+} as Meta<typeof StackExample>
 
-const Template = () => <StackExample />
+type Story = StoryObj<typeof StackExample>
 
-export const Default = Template.bind({})
+export const Default: Story = {}
