836005bcf0b55d84c6300a9171b2dd39d928aadb
7alip <talipaltas@gmail.com>
2023-06-05 23:57:26 +0200

    Fix caps dimensions

--- packages/ui/src/components/Caps/Caps.tsx
+++ packages/ui/src/components/Caps/Caps.tsx
@@ -89,8 +89,8 @@ export const Caps = forwardRef<CapsProps, 'div'>(
                 position={'absolute'}
                 top={'0px'}
                 height={'full'}
-                w={dimensions.shape}
-                h={dimensions.height}
+                w={`${dimensions.shape}px`}
+                h={`${dimensions.height}px`}
                 left={
                   flip
                     ? `${dimensions.image - dimensions.shape}px`
@@ -106,8 +106,8 @@ export const Caps = forwardRef<CapsProps, 'div'>(
               {/* Logo */}
               <chakra.svg
                 xmlns="http://www.w3.org/2000/svg"
-                width={dimensions.logo}
-                height={dimensions.logo}
+                width={`${dimensions.logo}px`}
+                height={`${dimensions.logo}px`}
                 position={'absolute'}
                 bottom={`${dimensions.padding}px`}
                 left={
@@ -147,14 +147,14 @@ export const Caps = forwardRef<CapsProps, 'div'>(
                 top={dimensions.padding}
                 bottom={dimensions.padding}
                 left={
-                  flip
+                  `${flip
                     ? dimensions.image + dimensions.shape
-                    : dimensions.padding
+                    : dimensions.padding}px`
                 }
                 right={
-                  flip
+                  `${flip
                     ? dimensions.padding
-                    : dimensions.image + dimensions.shape
+                    : dimensions.image + dimensions.shape}px`
                 }
                 spacing={`${dimensions.font / 2}px`}
                 userSelect={'none'}
