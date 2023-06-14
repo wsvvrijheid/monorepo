a392da5a228db10fc5c9630beff7d53d6b56388c
7alip <talipaltas@gmail.com>
2023-06-14 20:09:54 +0200

    Fix form locale switcher

--- packages/ui/src/admin/FormLocaleSwitcher/FormLocaleSwitcher.tsx
+++ packages/ui/src/admin/FormLocaleSwitcher/FormLocaleSwitcher.tsx
@@ -8,25 +8,32 @@ export const FormLocaleSwitcher = <T extends StrapiTranslatableModel>({
   models,
 }: {
   models: T[]
+  slug?: string
 }) => {
   const router = useRouter()
   const slug = router.query.slug as string
 
   return (
     <ButtonGroup>
-      {models?.map(model => (
-        <Button
-          as={Link}
-          key={model.id}
-          textTransform={'uppercase'}
-          href={{
-            pathname: router.pathname,
-            query: { ...router.query, id: model.id, slug },
-          }}
-        >
-          {model.locale}
-        </Button>
-      ))}
+      {models?.map(model => {
+        const href = slug
+          ? `/${slug}/${model.id}`
+          : {
+              pathname: router.pathname,
+              query: { ...router.query, id: model.id },
+            }
+
+        return (
+          <Button
+            as={Link}
+            key={model.id}
+            textTransform={'uppercase'}
+            href={href}
+          >
+            {model.locale}
+          </Button>
+        )
+      })}
     </ButtonGroup>
   )
 }
