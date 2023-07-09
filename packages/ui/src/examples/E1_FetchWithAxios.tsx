ae028552dbc80d7df030ea215d360757713816d9
GOZTURK7 <gokhan.89.ozturk@gmail.com>
2023-07-09 15:52:25 +0200

    Onboarding Fetch Examples E1 and E2 done

--- packages/ui/src/examples/E1_FetchWithAxios.tsx
+++ packages/ui/src/examples/E1_FetchWithAxios.tsx
@@ -1,23 +1,47 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react'
 
-import { Box } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
+import axios from 'axios'
 import { useRouter } from 'next/router'
+import * as qs from 'qs'
 
 import { API_URL } from '@wsvvrijheid/config'
 import { TOKEN } from '@wsvvrijheid/secrets'
-
-const BLOG_URL = `${API_URL}/api/blogs` // https://api.samenvvv.nl/api/blogs
+import { Blog } from '@wsvvrijheid/types'
 
 export const FetcWithAxios = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState<Blog[]>([])
   const { locale } = useRouter()
+
+  const headers = {
+    Authorization: 'Bearer ' + TOKEN,
+  }
+
+  const query = qs.stringify(
+    { locale: 'tr' },
+    {
+      encodeValuesOnly: true, // prettify URL
+    },
+  )
 
   useEffect(() => {
     // TODO: fetch blogs with axios by using the API_URL and TOKEN (Authorization header with Bearer token)
+    axios
+      .get(`https://api.wsvvrijheid.nl/api/blogs?${query}`, { headers })
+      .then(response => {
+        setBlogs(response.data.data)
+      })
+      .catch(error => console.log(error))
     // NOTE: Not every locale may have a blog, so you may need to change the locale to tr
     // REF: https://docs.strapi.io/dev-docs/plugins/i18n#getting-localized-entries-with-the-locale-parameter
   }, [])
 
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+  return (
+    <Box>
+      {blogs.map(blog => (
+        <h1 key={blog.id}>{blog.title}</h1>
+      ))}
+    </Box>
+  )
 }
--- packages/ui/src/examples/E2_FetchWithStrapiRequest.tsx
+++ packages/ui/src/examples/E2_FetchWithStrapiRequest.tsx
@@ -2,16 +2,47 @@
 import { useEffect, useState } from 'react'
 
 import { Box } from '@chakra-ui/react'
+import axios from 'axios'
+import qs from 'qs'
 
 import { strapiRequest } from '@wsvvrijheid/lib'
+import { TOKEN } from '@wsvvrijheid/secrets'
+import { Blog } from '@wsvvrijheid/types'
 
 export const FetchWithStrapiRequest = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState<Blog[]>([])
+
+  const query = qs.stringify(
+    { locale: 'tr' },
+    {
+      encodeValuesOnly: true, // prettify URL
+    },
+  )
+
+  const API_URL = `https://api.wsvvrijheid.nl/api/blogs?${query}`
+
+  const fetcher = (API_URL: string, token: string | undefined) => {
+    const headers = {
+      Authorization: 'Bearer ' + token,
+    }
+    axios
+      .get(API_URL, { headers })
+      .then(response => setBlogs(response.data.data))
+      .catch(err => console.log(err))
+  }
 
   useEffect(() => {
     // TODO: fetch blogs with fetcher by using the API_URL and TOKEN
     // Remember that fetcher is a wrapper around axios that adds the token and api url to the request
+    fetcher(API_URL, TOKEN)
   }, [])
 
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+  return (
+    <Box>
+      {/* TODO: Show only title of the blogs */}
+      {blogs.map(blog => (
+        <h1 key={blog.id}>{blog.title}</h1>
+      ))}
+    </Box>
+  )
 }
