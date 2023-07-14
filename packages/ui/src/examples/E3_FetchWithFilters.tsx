83b59ff53e62fef19ff072161ea749263969c41d
GOZTURK7 <gokhan.89.ozturk@gmail.com>
2023-07-14 10:05:24 +0200

    E3_FetchWithFilters done

--- packages/ui/src/examples/E3_FetchWithFilters.tsx
+++ packages/ui/src/examples/E3_FetchWithFilters.tsx
@@ -17,42 +17,14 @@ export const FetchWithFilters: FC<RequestFilterProps> = ({ initialValue }) => {
   const [blogs, setBlogs] = useState<Blog[]>([])
   const [titleFilter, setTitleFilter] = useState(initialValue)
 
-  const query = qs.stringify(
-    {
-      filters: {
-        title: {
-          $startsWith: titleFilter,
-        },
-      },
-      locale: "tr",
-      sort: ['title:desc'],
-    },
-    {
-      encodeValuesOnly: true, // prettify URL
-    },
-  )
-
-  // const API_URL = `https://api.wsvvrijheid.nl/api/blogs?${query}`
-
-  // const fetcher = (API_URL: string, token: string | undefined) => {
-  //   const headers = {
-  //     Authorization: 'Bearer ' + token,
-  //   }
-  //   axios
-  //     .get(API_URL, { headers })
-  //     .then(response => setBlogs(response.data.data))
-  //     .catch(err => console.log(err))
-  // }
-
   const inputHandler = (e: any) => {
-    console.log("e.target.value:" + e.target.value);
-    
+    console.log('e.target.value:' + e.target.value)
+
     setTitleFilter(e.target.value)
   }
 
   useEffect(() => {
-
-    const fetchData = async ()=>{
+    const fetchData = async () => {
       const response = await strapiRequest<Blog>({
         url: 'api/blogs',
         filters: {
@@ -60,15 +32,15 @@ export const FetchWithFilters: FC<RequestFilterProps> = ({ initialValue }) => {
             $startsWith: titleFilter,
           },
         },
-        // locale: "tr",
-        // sort: ['title:desc'],
-      });
+        locale: 'tr',
+        sort: ['title:desc'],
+      })
       setBlogs(response.data)
-      console.log(response);
+      console.log(response)
     }
-   
+
     fetchData()
-    
+
     // fetcher(API_URL, TOKEN)
     // TODO: fetch blogs with filterValue by using strapiRequest
     // NOTE: Changing filteredValue should trigger a new fetch.
@@ -77,12 +49,19 @@ export const FetchWithFilters: FC<RequestFilterProps> = ({ initialValue }) => {
   }, [titleFilter])
 
   // TODO Add Input to change titleFilter
-  return <Box>
-    <label htmlFor="search">Serach Blog By Title</label>
-    <input name="search" id="search" type="text" value={titleFilter} onChange={(e)=>inputHandler(e)}></input>
-    {blogs.map(blog => (
+  return (
+    <Box>
+      <label htmlFor="search">Serach Blog By Title</label>
+      <input
+        name="search"
+        id="search"
+        type="text"
+        value={titleFilter}
+        onChange={inputHandler}
+      ></input>
+      {blogs.map(blog => (
         <h1 key={blog.id}>{blog.title}</h1>
       ))}
-    
-  </Box>
+    </Box>
+  )
 }
