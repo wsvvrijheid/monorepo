8e27172f79886bbf3bb4be5a63795b89e1f57ddc
MustafaTRKYLMZ <cana66331@gmail.com>
2023-07-29 12:46:27 +0200

    bug fixed

--- packages/ui/src/admin/AuthModal/AuthModal.tsx
+++ packages/ui/src/admin/AuthModal/AuthModal.tsx
@@ -77,124 +77,119 @@ export const AuthModal = () => {
   }
 
   return (
-    <Box bg="red">
-      <Modal
-        // isCentered
-        isOpen={authModalDisclosure.isOpen}
-        onClose={authModalDisclosure.onClose}
-        size={'sm'}
-        scrollBehavior="inside"
-        closeOnOverlayClick={false}
-        closeOnEsc={false}
+    <Modal
+      isCentered
+      isOpen={authModalDisclosure.isOpen}
+      onClose={authModalDisclosure.onClose}
+      size={'sm'}
+      scrollBehavior="inside"
+      closeOnOverlayClick={false}
+      closeOnEsc={false}
+    >
+      <ModalOverlay />
+      <ModalContent maxW="95vw" h="full" p={{ base: 2, lg: 4 }}>
+        <ModalHeader color={'primary.500'}>Login</ModalHeader>
+        <ModalCloseButton />
+        {isAuthLoading && (
+          <Center>
+            <Spinner />
+          </Center>
+        )}
+        <ModalBody>
+          <Container maxW={{ base: 'full', lg: 300 }}>
+            <Stack
+              h="full"
+              w="full"
+              textAlign="center"
+              spacing={4}
+              justify="center"
+              pb={8}
+              pt={{ base: 8, lg: '50%' }}
+            >
+              <Navigate href="/">
+                <VStack textAlign="center" w={'full'}>
+                  <WAvatar size="2xl" src={`/images/wsvvrijheid-logo.svg`} />
 
-        // {...rest}
-      >
-        <ModalOverlay />
-        <ModalContent maxW="95vw" h="full" p={{ base: 2, lg: 4 }}>
-          <ModalHeader color={'primary.500'}>Login</ModalHeader>
-          <ModalCloseButton />
-          {isAuthLoading && (
-            <Center>
-              <Spinner />
-            </Center>
-          )}
-          <ModalBody>
-            <Container maxW={{ base: 'full', lg: 300 }}>
-              {/* right side (second container) */}
-              <Stack
-                h="full"
-                w="full"
-                textAlign="center"
-                spacing={4}
-                justify="center"
-                pb={8}
-                pt={{ base: 8, lg: '50%' }}
-              >
-                <Navigate href="/">
-                  <VStack textAlign="center" w={'full'}>
-                    <WAvatar size="2xl" src={`/images/wsvvrijheid-logo.svg`} />
+                  <Text fontSize="xl" color={'blue.500'} fontWeight={900}>
+                    WEES DE STEM <br />
+                    VOOR VRIJHEID
+                  </Text>
+                </VStack>
+              </Navigate>
 
-                    <Text fontSize="xl" color={'blue.500'} fontWeight={900}>
-                      WEES DE STEM <br />
-                      VOOR VRIJHEID
-                    </Text>
-                  </VStack>
-                </Navigate>
-
-                <Stack spacing={4} flex={1}>
-                  <Stack
-                    spacing={4}
-                    as="form"
-                    onSubmit={handleSubmit(handleSubmitSign)}
-                  >
-                    <FormItem
-                      w="full"
-                      name="identifier"
-                      label={t('login.email-or-username.title') as string}
-                      register={register}
-                      errors={errors}
-                    />
-                    <FormItem
-                      w="full"
-                      name="password"
-                      type="password"
-                      label={t('login.password.title') as string}
-                      autoComplete="current-password"
-                      register={register}
-                      errors={errors}
-                    />
-                    <Button
-                      isLoading={isAuthLoading || isRedirecting}
-                      w="full"
-                      type="submit"
-                    >
-                      {t('login.sign-in')}
-                    </Button>
-                    {loginMutation.isError &&
-                      ((loginMutation.error as any)?.response?.data?.type ===
-                      'unauthorized' ? (
-                        <Text fontSize={'sm'} color={'red.500'}>
-                          <Trans
-                            i18nKey="login.error.unauthorized"
-                            components={{
-                              a: (
-                                <Link
-                                  isExternal
-                                  href={'https://www.wsvvrijheid.nl/tr/contact'}
-                                  color="blue.500"
-                                />
-                              ),
-                            }}
-                          />
-                        </Text>
-                      ) : (
-                        <Text color="red.500" fontSize="sm">
-                          {(loginMutation.error as any)?.response?.data
-                            ?.message || 'An error occured'}
-                        </Text>
-                      ))}
-                  </Stack>
-                  {/* TODO Set session exp time */}
-
+              <Stack spacing={4} flex={1}>
+                <Stack
+                  spacing={4}
+                  as="form"
+                  onSubmit={handleSubmit(handleSubmitSign)}
+                >
+                  <FormItem
+                    w="full"
+                    name="identifier"
+                    label={t('login.email-or-username.title') as string}
+                    register={register}
+                    errors={errors}
+                  />
+                  <FormItem
+                    w="full"
+                    name="password"
+                    type="password"
+                    label={t('login.password.title') as string}
+                    autoComplete="current-password"
+                    register={register}
+                    errors={errors}
+                  />
                   <Button
-                    as={Navigate}
-                    href="/forgot-password"
-                    variant="link"
-                    size="sm"
+                    isLoading={isAuthLoading || isRedirecting}
+                    w="full"
+                    type="submit"
                   >
-                    {t('login.forgot-pass-header.title')}
+                    {t('login.sign-in')}
                   </Button>
+                  {loginMutation.isError &&
+                    ((loginMutation.error as any)?.response?.data?.type ===
+                    'unauthorized' ? (
+                      <Text fontSize={'sm'} color={'red.500'}>
+                        <Trans
+                          i18nKey="login.error.unauthorized"
+                          components={{
+                            a: (
+                              <Link
+                                isExternal
+                                href={'https://www.wsvvrijheid.nl/tr/contact'}
+                                color="blue.500"
+                              />
+                            ),
+                          }}
+                        />
+                      </Text>
+                    ) : (
+                      <Text color="red.500" fontSize="sm">
+                        {(loginMutation.error as any)?.response?.data
+                          ?.message || 'An error occured'}
+                      </Text>
+                    ))}
                 </Stack>
+                {/* TODO Set session exp time */}
 
-                <Text fontSize={'xs'}>
-                  Wsvvrijheid &copy; {new Date().getFullYear()} All rights
-                  reserved
-                </Text>
+                <Button
+                  as={Navigate}
+                  href="/forgot-password"
+                  variant="link"
+                  size="sm"
+                >
+                  {t('login.forgot-pass-header.title')}
+                </Button>
               </Stack>
-            </Container>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </Box>
+
+              <Text fontSize={'xs'}>
+                Wsvvrijheid &copy; {new Date().getFullYear()} All rights
+                reserved
+              </Text>
+            </Stack>
+          </Container>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   )
 }
