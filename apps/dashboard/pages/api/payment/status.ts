e8dc46f0fd4d90890cf20a21c1a47b577c1713d8
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2023-08-08 01:55:21 +0200

    create a single function for donation stripe codes

--- apps/dashboard/pages/api/payment/status.ts
+++ apps/dashboard/pages/api/payment/status.ts
@@ -1,43 +1,18 @@
 import { NextApiHandler } from 'next'
+import Stripe from 'stripe'
 
-import { Mutation } from '@wsvvrijheid/lib'
-import { TOKEN } from '@wsvvrijheid/secrets'
-import { Donation, DonationUpdateInput, StrapiUrl } from '@wsvvrijheid/types'
+import { donationWebhook } from '@wsvvrijheid/stripe'
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const event = req.body
+    const event: Stripe.Event = req.body
     if (!event) {
       res.status(500).send('Something went wrong')
 
       return
     }
 
-    // check the checkout session status, if it's paid then update the donation status
-    if (event?.data?.object?.object === 'checkout.session') {
-      const status = event.data.object.payment_status
-      const checkoutSessionId = event.data.object.id
-      const donationId = event.data.object.success_url.split('&')[1].slice(3)
-
-      // Update donation status and stripe fields in database
-      await Mutation.put<Donation, DonationUpdateInput>(
-        'api/donates',
-        donationId,
-        {
-          status,
-          checkoutSessionId: checkoutSessionId as string,
-        },
-        TOKEN as string,
-      )
-      // Send email to customer
-      if (status === 'paid') {
-        await Mutation.post(
-          `api/donates/email/${donationId}` as StrapiUrl,
-          {},
-          TOKEN as string,
-        )
-      }
-    }
+    await donationWebhook(event)
 
     res.status(200).send('complete')
   } catch (err) {
---
+++ packages/stripe/src/donation/createCheckout.ts
@@ -0,0 +1,62 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { SITE_URL } from '@wsvvrijheid/config'
+import { Mutation } from '@wsvvrijheid/lib'
+import { TOKEN } from '@wsvvrijheid/secrets'
+import { Donation, DonationCreateInput } from '@wsvvrijheid/types'
+
+import { stripe } from '../initStripe'
+
+export const createCheckout = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+) => {
+  const { amount, name, email, type } = req.body
+  // Create blank donation in database
+  const donation = await Mutation.post<Donation, DonationCreateInput>(
+    'api/donates',
+    { name, email, amount },
+    TOKEN as string,
+  )
+
+  // Check if customer exists
+  const customer = await stripe.customers.list({
+    email,
+  })
+
+  let customerID = customer.data.length > 0 ? customer.data[0].id : ''
+  // Create customer if not exists
+  if (customer.data.length === 0) {
+    const newCustomer = await stripe.customers.create({
+      email,
+      name,
+    })
+    customerID = newCustomer.id
+  }
+  // Create checkout session for subscription
+  const payment = await stripe.checkout.sessions.create({
+    payment_method_types: ['ideal', 'card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'eur',
+          product_data: {
+            name: 'Donatie',
+            metadata: {
+              strapi_id: donation.id,
+            },
+          },
+          unit_amount: amount * 100,
+          recurring: type === 'monthly' ? { interval: 'month' } : undefined,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: type === 'monthly' ? 'subscription' : 'payment',
+    customer: customerID,
+    success_url: `${SITE_URL}/donation/complete?status=success&id=${donation.id}&session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${SITE_URL}/donation/complete?status=cancel`,
+  })
+
+  res.status(200).send(payment.url)
+}
