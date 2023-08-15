41d835738b95c1e6f84e91f0543897fbb889d908
MustafaTRKYLMZ <cana66331@gmail.com>
2023-08-15 21:56:32 +0200

    image added to editmodel

--- packages/ui/src/data/schemas/userFeedback.ts
+++ packages/ui/src/data/schemas/userFeedback.ts
@@ -8,10 +8,16 @@ export const userFeedbackSchema = yup.object({
   comment: yup.string(),
   point: yup.string(),
   site: yup.string(),
+  image: yup.mixed(),
 })
 
 export const userFeedbackFields: FormFields<UserFeedback> = [
   { name: 'comment' },
   { name: 'point' },
   { name: 'site' },
+  {
+    name: 'image',
+    type: 'file',
+    group: { label: 'Image', value: 'image', name: 'media' },
+  },
 ]
