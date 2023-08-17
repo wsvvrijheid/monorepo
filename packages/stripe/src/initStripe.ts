4cf978f81c4c2da62e03edd232aeb21457f4abd0
7alip <talipaltas@gmail.com>
2023-08-17 14:07:44 +0200

    Update nextjs and stripe

--- packages/stripe/src/initStripe.ts
+++ packages/stripe/src/initStripe.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe'
 import { getSecret } from '@wsvvrijheid/secrets'
 
 export const stripe = new Stripe(getSecret('STRIPE_KEY') as string, {
-  apiVersion: '2022-11-15',
+  apiVersion: '2023-08-16',
 })
