9881a632f111fcde4a8f595123962db916f07be6
7alip <talipaltas@gmail.com>
2023-09-06 13:02:22 +0200

    Fix invalid filters for Strapi request

--- packages/ui/src/admin/ModelFiltersBar/ModelFiltersBar.tsx
+++ packages/ui/src/admin/ModelFiltersBar/ModelFiltersBar.tsx
@@ -9,6 +9,8 @@ type ModelFiltersBarProps = {
   setStatus: (status: ApprovalStatus) => void
   published: string
   setPublished: (published: string) => void
+  showApprovalStatus?: boolean
+  showPublicationState?: boolean
 }
 
 export const ModelFiltersBar: FC<ModelFiltersBarProps> = ({
@@ -16,7 +18,11 @@ export const ModelFiltersBar: FC<ModelFiltersBarProps> = ({
   setStatus,
   published,
   setPublished,
+  showApprovalStatus,
+  showPublicationState,
 }) => {
+  if (!showApprovalStatus && !showPublicationState) return null
+
   return (
     <Stack
       direction={{ base: 'column', lg: 'row' }}
@@ -27,37 +33,43 @@ export const ModelFiltersBar: FC<ModelFiltersBarProps> = ({
       overflowX={'auto'}
       flexShrink={0}
     >
-      <RadioGroup
-        as={HStack}
-        spacing={4}
-        colorScheme={'primary'}
-        value={status || 'all'}
-        onChange={val => setStatus(val as ApprovalStatus)}
-      >
-        <Radio value={'all'}>All</Radio>
-        <Radio value={'approved'}>Approved</Radio>
-        <Radio value={'pending'}>Pending</Radio>
-        <Radio value={'rejected'}>Rejected</Radio>
-      </RadioGroup>
+      {showApprovalStatus && (
+        <RadioGroup
+          as={HStack}
+          spacing={4}
+          colorScheme={'primary'}
+          value={status || 'all'}
+          onChange={val => setStatus(val as ApprovalStatus)}
+        >
+          <Radio value={'all'}>All</Radio>
+          <Radio value={'approved'}>Approved</Radio>
+          <Radio value={'pending'}>Pending</Radio>
+          <Radio value={'rejected'}>Rejected</Radio>
+        </RadioGroup>
+      )}
 
-      <Divider
-        display={{ base: 'none', lg: 'block' }}
-        mx={8}
-        orientation="vertical"
-      />
-      <Divider display={{ base: 'block', lg: 'none' }} />
+      {showPublicationState && (
+        <>
+          <Divider
+            display={{ base: 'none', lg: 'block' }}
+            mx={8}
+            orientation="vertical"
+          />
+          <Divider display={{ base: 'block', lg: 'none' }} />
 
-      <RadioGroup
-        colorScheme={'primary'}
-        as={HStack}
-        spacing={4}
-        value={published || 'true'}
-        onChange={val => setPublished(val)}
-      >
-        <Radio value={'all'}>All</Radio>
-        <Radio value={'true'}>Live</Radio>
-        <Radio value={'false'}>Draft</Radio>
-      </RadioGroup>
+          <RadioGroup
+            colorScheme={'primary'}
+            as={HStack}
+            spacing={4}
+            value={published || 'true'}
+            onChange={val => setPublished(val)}
+          >
+            <Radio value={'all'}>All</Radio>
+            <Radio value={'true'}>Live</Radio>
+            <Radio value={'false'}>Draft</Radio>
+          </RadioGroup>
+        </>
+      )}
     </Stack>
   )
 }
