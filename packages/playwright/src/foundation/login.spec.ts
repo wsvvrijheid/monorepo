f22acc4613bbf0d921e05670c2c786007b5cbfa3
7alip <talipaltas@gmail.com>
2023-09-19 08:00:16 +0200

    Add start script

--- packages/playwright/src/foundation/login.spec.ts
+++ packages/playwright/src/foundation/login.spec.ts
@@ -1,27 +1,20 @@
 import { expect, test } from '@playwright/test'
 import dotenv from 'dotenv'
 dotenv.config()
-console.log('url', process.env['PLAYWRIGHT_TEST_BASE_URL'])
 
 test('login foundation', async ({ page }) => {
-  console.log('url', process.env['PLAYWRIGHT_TEST_BASE_URL'])
-  await page.goto(
-    `https://wsvvrijheid-git-${process.env['PLAYWRIGHT_TEST_BASE_URL']}-wsvvrijheid.vercel.app/`,
-    {
-      waitUntil: 'domcontentloaded',
-    },
-  )
+  await page.goto(`http://localhost:3001`, {
+    waitUntil: 'domcontentloaded',
+  })
   await page.getByRole('button', { name: 'Allow' }).click()
   await page.getByRole('link', { name: 'Sign in' }).click()
   await page.getByTestId('input-email').click()
-  await page.getByTestId('input-email').fill(process.env['username'] as string)
+  await page.getByTestId('input-email').fill(process.env['USERNAME'] as string)
   await page.getByTestId('input-password').click()
   await page
     .getByTestId('input-password')
-    .fill(process.env['password'] as string)
+    .fill(process.env['PASSWORD'] as string)
   await page.getByTestId('button-submit-login').click()
   await page.waitForNavigation()
-  await expect(page).toHaveURL(
-    `https://wsvvrijheid-git-${process.env['PLAYWRIGHT_TEST_BASE_URL']}-wsvvrijheid.vercel.app/`,
-  )
+  await expect(page).toHaveURL(`http://localhost:3000`)
 })
--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,6 @@ import { defineConfig, devices } from '@playwright/test'
 import dotenv from 'dotenv'
 dotenv.config()
 
-// Use process.env.PORT by default and fallback to port 3000
-const PORT = process.env.PORT || 3000
-
 // Set webServer.url and use.baseURL with the location of the WebServer respecting the correct set port
 // const baseURL = `http://127.0.0.1:${PORT}`
 
