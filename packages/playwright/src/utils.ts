46462083a66c9de13cd0fc189d71f43248e44c2e
7alip <talipaltas@gmail.com>
2023-09-25 03:56:00 +0200

    Fix CI url

--- packages/playwright/src/utils.ts
+++ packages/playwright/src/utils.ts
@@ -3,7 +3,7 @@ import { ProjectApp } from './types'
 
 export const getVercelUrl = (project: ProjectApp) => {
   if (process.env['CI'] === 'true') {
-    return `https://${project}-git-dev-wsvvrijheid.vercel.app/`
+    return `https://${project}-git-dev-wsvvrijheid.vercel.app`
   }
 
   return `http://localhost:${ports[project]}`
