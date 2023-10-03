7ccd219d0d3c5e2e5d6bc684be3b122154532761
Hasan ! <hasancomcee@gmail.com>
2023-10-04 01:01:36 +0200

    E3_FetchWithFilters issue has been completed.

--- packages/ui/src/examples/E3_FetchWithFilters.tsx
+++ packages/ui/src/examples/E3_FetchWithFilters.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { FC, useEffect, useState } from 'react'
 
-import { Box } from '@chakra-ui/react'
+import { Box, OrderedList, ListItem, Input } from '@chakra-ui/react'
 
 import { strapiRequest } from '@wsvvrijheid/lib'
+import { Blog } from '@wsvvrijheid/types'
 
 export type RequestFilterProps = {
   initialValue: string
@@ -18,8 +19,40 @@ export const FetchWithFilters: FC<RequestFilterProps> = ({ initialValue }) => {
     // TODO: Fetch blogs with by providing `title` filter to strapiRequest (title: { $containsi: title })
     // NOTE: Changing `title` should trigger a new fetch.
     // BONUS: You can play by using other arguments of strapiRequest (sort, populate,  fields etc).
-  }, [])
 
-  // TODO Add Input to change titleFilter
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+    async function getBlogs() {
+      await strapiRequest<Blog>({
+        endpoint: 'blogs',
+        filters: {
+          title: { $containsi: title },
+        },
+        sort: ['updatedAt:asc'],
+      }).then((response: any) => {
+        setBlogs(response.data)
+      })
+    }
+    getBlogs()
+  }, [title])
+
+  const handleChange = (e: any) => setTitle(e.target.value)
+
+  return (
+    <Box boxShadow="lg" p="5">
+      {/* TODO: Show only title of the blogs */}
+      {/* TODO Add Input to change titleFilter */}
+
+      <Input
+        value={title}
+        onChange={handleChange}
+        placeholder="Title filter"
+        my={2}
+      />
+
+      <OrderedList>
+        {blogs.map(({ id, title }) => (
+          <ListItem key={id}>{title}</ListItem>
+        ))}
+      </OrderedList>
+    </Box>
+  )
 }
