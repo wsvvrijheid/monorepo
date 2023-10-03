41c0d92d016a0abe4c745babeb53f56684f1ffc1
Hasan ! <hasancomcee@gmail.com>
2023-10-03 18:40:10 +0200

    The responsive card completed.

--- packages/ui/src/examples/E0_ResponsiveCard.tsx
+++ packages/ui/src/examples/E0_ResponsiveCard.tsx
@@ -4,28 +4,33 @@ export const ResponsiveCard = () => {
   const randomPic = 'https://picsum.photos/600/400'
 
   return (
-    <Flex>
+    <Flex
+      w={{ md: '100%' }}
+      p={{ base: 5, md: 0 }}
+      flexDir={{ base: 'column', md: 'row' }}
+      boxShadow="md"
+      bg="white"
+    >
       <Stack>
-        <Image src={randomPic} alt="random pic" />
+        <Image
+          src={randomPic}
+          alt="randomPicDesc"
+          objectFit="cover"
+          w={{ base: '70%', md: '40%' }}
+          m={{ base: 'auto', md: 'none' }}
+          aspectRatio={{ base: 1, md: 'none' }}
+          borderRadius={{ base: 'full', md: 'none' }}
+        />
       </Stack>
-
-      <Stack>
-        <Heading as="h1" size="lg" mb={5} color="#000" fontWeight="500">
-          This is Heading...
+      <Stack dir="column" m={4}>
+        <Heading fontSize="xl" fontWeight="bold">
+          This is Heading... Lorem ipsum dolor sit.
         </Heading>
-        <Text>
+        <Text mt={2}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
           dolores totam blanditiis soluta quae delectus doloremque velit sit
           corrupti iure natus nobis nesciunt, tempore repellat cupiditate culpa
-          voluptatum expedita. Ducimus sapiente exercitationem repellendus et
-          aspernatur. Aspernatur expedita a ea non iure aperiam assumenda
-          praesentium beatae numquam repellat, excepturi impedit voluptatem!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          dolores totam blanditiis soluta quae delectus doloremque velit sit
-          corrupti iure natus nobis nesciunt, tempore repellat cupiditate culpa
-          voluptatum expedita. Ducimus sapiente exercitationem repellendus et
-          aspernatur. Aspernatur expedita a ea non iure aperiam assumenda
-          praesentium beatae numquam repellat, excepturi impedit voluptatem!
+          voluptatum expedita.
         </Text>
       </Stack>
     </Flex>
