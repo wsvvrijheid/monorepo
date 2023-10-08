c20b136462b59049e51156eeeb5ee90c7c00fe6d
Hakan <s.hakanayata@gmail.com>
2023-10-08 21:22:11 +0200

    types add

--- packages/ui/src/examples/E2_FetchWithStrapiRequest.tsx
+++ packages/ui/src/examples/E2_FetchWithStrapiRequest.tsx
@@ -1,17 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react'
 
-import { Box } from '@chakra-ui/react'
+import { Box, ListItem, UnorderedList } from '@chakra-ui/react'
 
 import { strapiRequest } from '@wsvvrijheid/lib'
+import { Blog, StrapiModel } from '@wsvvrijheid/types'
 
 export const FetchWithStrapiRequest = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState<Blog[]>([])
 
   useEffect(() => {
     // TODO: fetch blogs with strapiRequest by using the API_URL and TOKEN
     // Remember that fetcher is a wrapper around axios that adds the token and api url to the request
+    strapiRequest<Blog>({ endpoint: "blogs" })
+      .then(data => {
+        setBlogs(data.data)
+      })
   }, [])
 
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+  return <Box>
+    {/* TODO: Show only title of the blogs */}
+    <UnorderedList>
+      {blogs?.map((blog) => {
+        return (<ListItem key={blog.id}>{blog.title}</ListItem>)
+      })}
+    </UnorderedList>
+  </Box>
 }
--- packages/ui/src/examples/E3_FetchWithFilters.tsx
+++ packages/ui/src/examples/E3_FetchWithFilters.tsx
@@ -1,25 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { FC, useEffect, useState } from 'react'
 
-import { Box } from '@chakra-ui/react'
+import { Box, ListItem, UnorderedList } from '@chakra-ui/react'
 
 import { strapiRequest } from '@wsvvrijheid/lib'
+import { Blog } from '@wsvvrijheid/types'
 
 export type RequestFilterProps = {
   initialValue: string
 }
 
 export const FetchWithFilters: FC<RequestFilterProps> = ({ initialValue }) => {
-  const [blogs, setBlogs] = useState([])
-  const [title, setTitle] = useState(initialValue)
+  const [blogs, setBlogs] = useState<Blog[]>([])
+  const [title, setTitle] = useState<string>(initialValue)
 
   useEffect(() => {
     // REF: https://docs.strapi.io/dev-docs/api/rest/filters-locale-publication#filtering
     // TODO: Fetch blogs with by providing `title` filter to strapiRequest (title: { $containsi: title })
+    strapiRequest<Blog>({ endpoint: "blogs", filters: { title: { $eq: `The Hero` } } })
+      .then(data => setBlogs(data.data))
     // NOTE: Changing `title` should trigger a new fetch.
     // BONUS: You can play by using other arguments of strapiRequest (sort, populate,  fields etc).
   }, [])
 
   // TODO Add Input to change titleFilter
-  return <Box>{/* TODO: Show only title of the blogs */}</Box>
+  return <Box>{/* TODO: Show only title of the blogs */}
+    <UnorderedList>
+      {blogs?.map((blog) => {
+        return (<ListItem key={blog.id}>{blog.title}</ListItem>)
+      })}
+    </UnorderedList>
+  </Box>
 }
