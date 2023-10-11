0af022232d930c716706e3a8e508b690818d8b61
Hasan ! <hasancomcee@gmail.com>
2023-10-11 08:22:36 +0200

    E5_FetchWithCustomQuery issue has been completed.

--- packages/ui/src/examples/E5_FetchWithCustomQuery.tsx
+++ packages/ui/src/examples/E5_FetchWithCustomQuery.tsx
@@ -3,12 +3,35 @@
 import { useState } from 'react'
 
 import { Input, Stack } from '@chakra-ui/react'
-import { useQuery } from '@tanstack/react-query'
+import { QueryClient, useQuery } from '@tanstack/react-query'
+import { Blog, StrapiCollectionResponse } from '@wsvvrijheid/types'
+import { RequestCollectionArgs, strapiRequest } from '@wsvvrijheid/lib'
 
 const fetchBlogs = async (title?: string) => {
   // TODO: Use strapiRequest
   // TODO: Add title filter
-  // Return blogs array instead of StrapiCollectionResponse ({data, meta})
+  // TODO: Return blogs array instead of StrapiCollectionResponse ({data, meta})
+
+  const queryClient = new QueryClient()
+
+  const args: RequestCollectionArgs = {
+    endpoint: 'blogs',
+    locale: 'tr',
+    // sort: ['date:desc'],
+    filters: {
+      title: { $containsi: title },
+    },
+    // pageSize: 1,
+  }
+
+  const queryKey = Object.values(args)
+
+  await queryClient.prefetchQuery(queryKey, () => strapiRequest<Blog>(args))
+
+  const blogsResponse =
+    queryClient.getQueryData<StrapiCollectionResponse<Blog[]>>(queryKey)
+
+  return blogsResponse?.data
 }
 
 const useBlogsQuery = (title?: string) => {
@@ -26,8 +49,15 @@ export const FetchWithCustomQuery = () => {
 
   return (
     <Stack>
-      <Input placeholder="Blog title" value={title} />
+      <Input
+        placeholder="Blog title"
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
       {/* TODO: Render titles */}
+      {isLoaded && (
+        <ul>{data?.map(blog => <li key={blog.id}>{blog.title}</li>)}</ul>
+      )}
     </Stack>
   )
 }
