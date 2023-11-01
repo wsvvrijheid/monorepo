b0f9daac4d6739316f1d142acc7417c04f529103
7alip <talipaltas@gmail.com>
2023-11-01 23:21:16 +0100

    Update hashtag sentences

--- apps/dashboard/pages/api/kv/hashtag-sentences.ts
+++ apps/dashboard/pages/api/kv/hashtag-sentences.ts
@@ -1,9 +1,9 @@
 import { kv } from '@vercel/kv'
-import { unsealData } from 'iron-session/edge'
+// import { unsealData } from 'iron-session/edge'
 import { NextRequest, NextResponse } from 'next/server'
 
-import { sessionOptions } from '@wsvvrijheid/secrets'
-import { Auth, RoleType } from '@wsvvrijheid/types'
+// import { sessionOptions } from '@wsvvrijheid/secrets'
+// import { Auth, RoleType } from '@wsvvrijheid/types'
 
 export const config = {
   runtime: 'edge',
@@ -11,28 +11,27 @@ export const config = {
 
 const handler = async (req: NextRequest) => {
   const method = req.method
-
-  // Allow users to read the data without authentication
-  // but require admin role for all other mutations
-  if (method !== 'GET') {
-    const { user } = await unsealData<Auth>(
-      req.cookies.get('iron-session')?.value as string,
-      { password: sessionOptions.password },
-    )
-
-    const allowedRoles: RoleType[] = [
-      'admin',
-      'contentmanager',
-      'contentmanager_translator',
-    ]
-    const isAllowed = user?.roles?.some(role => allowedRoles.includes(role))
-
-    if (!isAllowed) {
-      throw new Error('Unauthorized')
-    }
-  }
-
   try {
+    // Allow users to read the data without authentication
+    // but require admin role for all other mutations
+    // if (method !== 'GET') {
+    //   const { user } = await unsealData<Auth>(
+    //     req.cookies.get('iron-session')?.value as string,
+    //     { password: sessionOptions.password },
+    //   )
+
+    //   const allowedRoles: RoleType[] = [
+    //     'admin',
+    //     'contentmanager',
+    //     'contentmanager_translator',
+    //   ]
+    //   const isAllowed = user?.roles?.some(role => allowedRoles.includes(role))
+
+    //   if (!isAllowed) {
+    //     throw new Error('Unauthorized')
+    //   }
+    // }
+
     if (method === 'POST') {
       try {
         const { hashtagId, value } = await req.json()
@@ -41,7 +40,7 @@ const handler = async (req: NextRequest) => {
 
         return NextResponse.json(response)
       } catch (error) {
-        console.error(error)
+        console.error('Create sentence error', error)
         throw error
       }
     }
@@ -54,7 +53,7 @@ const handler = async (req: NextRequest) => {
 
         return NextResponse.json(response)
       } catch (error) {
-        console.error(error)
+        console.error('Update sentence error', error)
         throw error
       }
     }
@@ -68,7 +67,7 @@ const handler = async (req: NextRequest) => {
 
         return NextResponse.json(result)
       } catch (error) {
-        console.error(error)
+        console.error('Delete sentence error', error)
         throw error
       }
     }
