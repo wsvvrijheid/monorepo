9e093829ad88c6decd54e6b2d18de2daf25c4e32
7alip <talipaltas@gmail.com>
2023-11-06 03:49:49 +0100

    Update next.config

--- packages/config/next.config.mjs
+++ packages/config/next.config.mjs
@@ -3,14 +3,6 @@
  **/
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: [
-    '@wsvvrijheid/config',
-    '@wsvvrijheid/context',
-    '@wsvvrijheid/lib',
-    '@wsvvrijheid/services',
-    '@wsvvrijheid/ui',
-    '@wsvvrijheid/utils',
-  ],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'nl', 'tr'],
@@ -25,31 +17,6 @@ const nextConfig = {
   images: {
     deviceSizes: [320, 480, 720, 1080],
     imageSizes: [150],
-    domains: [
-      'aktifhaber.com',
-      'www.turkishminute.com',
-      'api.samenvvv.nl',
-      'pbs.twimg.com',
-      'api.wsvvrijheid.nl',
-      'admin.wsvvrijheid.nl',
-      'localhost',
-      'amnesty.imgix.net',
-      'boldmedya.com',
-      'dekanttekening.nl',
-      'cdn.nos.nl',
-      'www.rtlnieuws.nl',
-      'image.writeclouds.com',
-      'www.tr724.com',
-      'images0.persgroep.net',
-      'image.shaber3.com',
-      'ipa.news',
-      'static.wixstatic.com',
-      'www.amnesty.org',
-    ],
-  },
-  modularizeImports: {
-    'date-fns': { transform: 'date-fns/{{member}}' },
-    lodash: { transform: 'lodash/{{member}}' },
   },
 }
 
