a52134ac79e33302112b9433fa9f75a7256060b4
MustafaTRKYLMZ <cana66331@gmail.com>
2023-11-25 12:54:13 +0100

    getSku updated

--- packages/utils/src/getSku.ts
+++ packages/utils/src/getSku.ts
@@ -1,17 +1,6 @@
 export const getSku = (id: number) => {
-  let sku = ''
   const year = new Date().getFullYear()
-  if (0 <= id && id <= 9) {
-    sku = `W${year}-00${id}`
+  const paddedId = id.toString().padStart(3, '0')
 
-    return sku
-  } else if (10 <= id && id <= 99) {
-    sku = `W${year}-0${id}`
-
-    return sku
-  } else {
-    sku = `W${year}-${id}`
-
-    return sku
-  }
+  return `W${year}-${paddedId}`
 }
