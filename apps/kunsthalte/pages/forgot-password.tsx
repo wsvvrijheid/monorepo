687497e190c990eabb5a08e567cac1ea3acc5aae
7alip <talipaltas@gmail.com>
2023-12-02 22:34:18 +0100

    Restore forgot password page

--- apps/kunsthalte/pages/forgot-password.tsx
+++ apps/kunsthalte/pages/forgot-password.tsx
@@ -1,3 +1,33 @@
-import { forgotPasswordRouter } from '@wsvvrijheid/services/src/api/forgot-password'
+import { Box } from '@chakra-ui/react'
+import { GetStaticPropsContext } from 'next'
+import { useTranslation } from 'next-i18next'
 
-export default forgotPasswordRouter
+import { ssrTranslations } from '@wsvvrijheid/services/ssrTranslations'
+import { StrapiLocale } from '@wsvvrijheid/types'
+import { ForgotPasswordForm } from '@wsvvrijheid/ui'
+
+import { Layout } from '../components'
+
+const ForgotPassword = () => {
+  const { t } = useTranslation()
+
+  return (
+    <Layout seo={{ title: t('password') }}>
+      <Box minH="inherit">
+        <ForgotPasswordForm />
+      </Box>
+    </Layout>
+  )
+}
+
+export default ForgotPassword
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const locale = context.locale as StrapiLocale
+
+  return {
+    props: {
+      ...(await ssrTranslations(locale)),
+    },
+  }
+}
