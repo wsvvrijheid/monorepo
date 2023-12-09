33dd4b2faaad367787355fcd2204bb38d8158d17
7alip <talipaltas@gmail.com>
2023-12-09 01:07:59 +0100

    Update getSecret function

--- packages/secrets/src/index.ts
+++ packages/secrets/src/index.ts
@@ -1,14 +1,14 @@
 const secrets = {
-  COOKIE_PASSWORD: process.env['SECRET_COOKIE_PASSWORD'],
-  DEEPL_API_KEY: process.env['DEEPL_API_KEY'],
-  RECAPTCHA_SECRET_KEY: process.env['RECAPTCHA_SECRET_KEY'],
-  STRIPE_KEY: process.env['STRIPE_KEY'],
-  TOKEN: process.env['API_TOKEN'],
+  COOKIE_PASSWORD: process.env['SECRET_COOKIE_PASSWORD'] as string,
+  DEEPL_API_KEY: process.env['DEEPL_API_KEY'] as string,
+  RECAPTCHA_SECRET_KEY: process.env['RECAPTCHA_SECRET_KEY'] as string,
+  STRIPE_KEY: process.env['STRIPE_KEY'] as string,
+  TOKEN: process.env['API_TOKEN'] as string,
 }
 
 type Secrets = keyof typeof secrets
 
-export const getSecret = (key: Secrets): string => {
+export const getSecret = (key: Secrets) => {
   if (typeof window !== 'undefined') {
     // console.error(key + ' secret should only be used on the server')
 
