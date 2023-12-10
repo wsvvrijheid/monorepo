4e662369173d20ae411eada1ea81b715a6067409
MustafaTRKYLMZ <cana66331@gmail.com>
2023-12-10 10:40:51 +0100

    assetstrackings list added

--- apps/dashboard/pages/assets/[id].tsx
+++ apps/dashboard/pages/assets/[id].tsx
@@ -1,27 +1,71 @@
+import { useEffect, useState } from 'react'
+
 import {
   Accordion,
   AccordionButton,
   AccordionIcon,
   AccordionItem,
   AccordionPanel,
+  MenuItem,
   Stack,
   Text,
+  useDisclosure,
+  useUpdateEffect,
 } from '@chakra-ui/react'
 import { GetServerSidePropsContext } from 'next'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
+import { FaArrowDown, FaArrowUp } from 'react-icons/fa'
 
 import { useStrapiRequest } from '@wsvvrijheid/services'
 import { ssrTranslations } from '@wsvvrijheid/services/ssrTranslations'
-import { Asset, StrapiLocale } from '@wsvvrijheid/types'
-import { AdminLayout, ModelEditForm } from '@wsvvrijheid/ui'
+import { Asset, StrapiLocale, AssetsTracking, Sort } from '@wsvvrijheid/types'
+import {
+  AdminLayout,
+  DataTable,
+  ModelEditForm,
+  ModelEditModal,
+  PageHeader,
+  useColumns,
+} from '@wsvvrijheid/ui'
 
 const AssetPage = () => {
   const { t } = useTranslation()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { locale, query } = useRouter()
 
-  const { query } = useRouter()
+  const columns = useColumns<AssetsTracking>()
 
+  const [selectedAssetsTrackingId, setSelectedAssetsTrackingId] =
+    useState<number>()
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [pageSize, setPageSize] = useState<number>(20)
+  const [searchTerm, setSearchTerm] = useState<string>()
+  const [sort, setSort] = useState<Sort>()
+
+  const handleSearch = (search?: string) => {
+    search ? setSearchTerm(search) : setSearchTerm(undefined)
+  }
   const id = Number(query.id as string)
+
+  const assetsTrackingsQuery = useStrapiRequest<AssetsTracking>({
+    endpoint: 'assets-trackings',
+    filters: {
+      asset: { id: { $eq: id } },
+      ...(searchTerm && { [`title_${locale}`]: { $containsi: searchTerm } }),
+    },
+    sort,
+    page: currentPage || 1,
+    pageSize: 100,
+    locale,
+  })
+  useUpdateEffect(() => {
+    assetsTrackingsQuery.refetch()
+  }, [locale, searchTerm, sort])
+
+  const assetsTrackings = assetsTrackingsQuery?.data?.data || []
+  const pageCount = assetsTrackingsQuery?.data?.meta?.pagination?.pageCount || 0
+  const totalCount = assetsTrackingsQuery?.data?.meta?.pagination?.total || 0
 
   const { data, isLoading, refetch } = useStrapiRequest<Asset>({
     endpoint: 'assets',
@@ -30,12 +74,38 @@ const AssetPage = () => {
 
   const asset = data?.data
 
+  const handleRowClick = (index: number, id: number) => {
+    setSelectedAssetsTrackingId(id)
+  }
+
+  useEffect(() => {
+    if (selectedAssetsTrackingId) {
+      onOpen()
+    }
+  }, [selectedAssetsTrackingId])
+
+  const handleClose = () => {
+    onClose()
+    setSelectedAssetsTrackingId(undefined)
+  }
+
   return (
     <AdminLayout
       seo={{ title: t('foundation.assets') }}
       isLoading={isLoading}
       hasBackButton
     >
+      {selectedAssetsTrackingId && (
+        <ModelEditModal<AssetsTracking>
+          title={'Assets Trackings'}
+          endpoint="assets-trackings"
+          id={selectedAssetsTrackingId}
+          isOpen={isOpen}
+          onClose={handleClose}
+          onSuccess={refetch}
+          size={'5xl'}
+        />
+      )}
       <Stack spacing={8} p={6}>
         <Accordion
           size={'lg'}
@@ -72,6 +142,47 @@ const AssetPage = () => {
 TODO
 TRACKING MUST BE HERE
 */}
+          <AccordionItem>
+            <AccordionButton
+              justifyContent="space-between"
+              _activeStep={{ bg: 'gray.200' }}
+              cursor="pointer"
+              fontSize="lg"
+              bg={'white'}
+              rounded={'md'}
+              fontWeight={600}
+              shadow={'sm'}
+            >
+              <Text>Asset Tracking</Text>
+              <AccordionIcon ml={'auto'} />
+            </AccordionButton>
+            <AccordionPanel mt={4} bg={'white'} rounded={'md'}>
+              <PageHeader
+                onSearch={handleSearch}
+                sortMenu={[
+                  <MenuItem key="asc" icon={<FaArrowUp />}>
+                    Name Asc
+                  </MenuItem>,
+                  <MenuItem key="desc" icon={<FaArrowDown />}>
+                    Name Desc
+                  </MenuItem>,
+                ]}
+              />
+
+              <DataTable<AssetsTracking>
+                columns={columns['assets-trackings']!}
+                currentPage={currentPage}
+                data={assetsTrackings}
+                onClickRow={handleRowClick}
+                onSort={setSort}
+                pageCount={pageCount}
+                pageSize={pageSize}
+                setCurrentPage={setCurrentPage}
+                setPageSize={setPageSize}
+                totalCount={totalCount}
+              />
+            </AccordionPanel>
+          </AccordionItem>
         </Accordion>
       </Stack>
     </AdminLayout>
