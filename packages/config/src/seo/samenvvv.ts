14349c0bf977f591cc4b7bb58221f11b1403d06a
7alip <talipaltas@gmail.com>
2023-12-16 02:30:51 +0100

    Add title templates

--- packages/config/src/seo/samenvvv.ts
+++ packages/config/src/seo/samenvvv.ts
@@ -5,11 +5,14 @@ import { Localize } from '@wsvvrijheid/types'
 export const samenvvv: Localize<NextSeoProps> = {
   en: {
     title: 'SamenVVV',
+    titleTemplate: '%s - SamenVVV',
   },
   nl: {
     title: 'SamenVVV',
+    titleTemplate: '%s - SamenVVV',
   },
   tr: {
     title: 'SamenVVV',
+    titleTemplate: '%s - SamenVVV',
   },
 }
