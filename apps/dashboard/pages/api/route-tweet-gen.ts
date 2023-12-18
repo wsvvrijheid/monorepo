1b0ae4b4facfa65ec1e7ce0a02e20b5348f6250f
Hakan <s.hakanayata@gmail.com>
2023-12-18 19:08:20 +0100

    refactor

--- apps/dashboard/pages/api/route-tweet-gen.ts
+++ apps/dashboard/pages/api/route-tweet-gen.ts
@@ -8,8 +8,10 @@ const openai = new OpenAI({
 })
 
 export default async function handler(req: Request) {
-  // Extract the `prompt` from the body of the request
   const { prompt, numberOfPosts, charLimit, language } = await req.json()
+  const postCount =
+    numberOfPosts > 0 || numberOfPosts < 40 ? numberOfPosts : 'two'
+  const characterLimit = charLimit > 0 || charLimit <= 150 ? charLimit : 200
   // Request the OpenAI API for the response based on the prompt
   const response = await openai.chat.completions.create({
     model: 'gpt-4',
@@ -22,15 +24,7 @@ export default async function handler(req: Request) {
       },
       {
         role: 'user',
-        content: `Given the following article, generate ${
-          numberOfPosts > 0
-            ? numberOfPosts
-            : numberOfPosts < 40
-              ? numberOfPosts
-              : 'one'
-        } ${language} posts for Twitter. The post shouldn't include any hashtags, and shouldn't exceed ${
-          charLimit > 0 ? charLimit : charLimit <= 150 ? charLimit : 200
-        } characters.
+        content: `Given the following article, generate ${postCount} ${language} posts for Twitter. The post shouldn't include any hashtags, and shouldn't exceed ${characterLimit} characters.
 Respond with a JSON array of posts ["post1", "post2" , ...]. Only respond with an array. Article:
 ${prompt}`,
       },
@@ -46,6 +40,3 @@ ${prompt}`,
 
   return new StreamingTextResponse(stream)
 }
-
-// sample news article
-// Tutuklu hasta Ramazan Taşkıran, yürüyemez hale geldi ancak tahliye edilmiyor! Yüzde 93 engelli, ağır böbrek hastası tutuklu Ramazan Taşkıran’ın artık yürüyemez hale geldiği öğrenildi. Kızı Meryem Taşkıran, konuyla ilgili son sosyal medya mesajında, “Babam, ‘İyi değilim, bir şeyler yapın’ diyeli neredeyse 20 gün oldu. Bugün görüşe iki jandarmanın kollarında gelmiş. Merdivenlerde kucaklarına alıyorlarmış. Bunları anlatmak çok zor; sesimiz ulaşması gereken yerlere ulaşıyor mu bilmiyoruz. Ümitsizliğe düşüyoruz artık. Babamın eve geldiği haberini duymak istiyoruz. Babamı tahliye edin artık. Ev hapsi verin, kelepçeyle evine gönderin.” ifadelerini kullandı. Ramazan Taşkıran, bir derneğe üye olduğu gerekçesiyle yapılan yargılamalar sonrası 9 yıl hapis cezasına çarptırılmıştı. Günde 15 saat diyalize girmek zorunda olan Ramazan Taşkıran için ‘reviri olan bir cezaevinde kalabilir’ raporu verilmişti.
