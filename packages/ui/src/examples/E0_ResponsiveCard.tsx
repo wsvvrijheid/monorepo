2e3df61ca2ad225c4f720cd7b18ee1479d8f0ddf
burakndrlnd <burak.ndrlnd@gmail.com>
2023-12-27 21:39:17 +0300

    updated.

--- packages/ui/src/examples/E0_ResponsiveCard.tsx
+++ packages/ui/src/examples/E0_ResponsiveCard.tsx
@@ -1,31 +1,55 @@
-import { Flex, Image, Stack, Heading, Text } from '@chakra-ui/react'
+import { Flex, Image, Stack, Heading, Text, useBreakpointValue } from '@chakra-ui/react'
 
 export const ResponsiveCard = () => {
   const randomPic = 'https://picsum.photos/600/400'
 
+  const horizontalBuild = {
+    baseFlex: {
+      flexDirection: 'row',
+      w: '100%',
+      h: '100%',
+      justifyContent: 'start',
+      alignItems: 'start'
+    },
+    image: {
+      w: '300px',
+      h: '100%',
+      objectFit: 'cover'
+    }
+  };
+
+  const verticalBuild = {
+    baseFlex: {
+      flexDirection: 'column',
+      w: '347px',
+      h: '707px',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    image: {
+      boxSize: '200px',
+      borderRadius: 'full'
+    }
+  }
+
+  const props = useBreakpointValue({
+    sm: horizontalBuild,
+    base: verticalBuild,
+  });
+
   return (
-    <Flex>
-      <Stack>
-        <Image src={randomPic} alt="random pic" />
+    <Flex m={8} sx={props.baseFlex} >
+      <Stack flexShrink={0} >
+        <Image sx={props.image} src={randomPic} alt="random pic"
+        />
       </Stack>
 
-      <Stack>
-        <Heading as="h1" size="lg" mb={5} color="#000" fontWeight="500">
-          This is Heading...
+      <Stack p={8} flexGrow={1}>
+        <Heading as="h1" mb={5} color="#000" fontWeight="500" fontSize="xl">
+          The standard Lorem Ipsum passage, used since the 1500s
         </Heading>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          dolores totam blanditiis soluta quae delectus doloremque velit sit
-          corrupti iure natus nobis nesciunt, tempore repellat cupiditate culpa
-          voluptatum expedita. Ducimus sapiente exercitationem repellendus et
-          aspernatur. Aspernatur expedita a ea non iure aperiam assumenda
-          praesentium beatae numquam repellat, excepturi impedit voluptatem!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          dolores totam blanditiis soluta quae delectus doloremque velit sit
-          corrupti iure natus nobis nesciunt, tempore repellat cupiditate culpa
-          voluptatum expedita. Ducimus sapiente exercitationem repellendus et
-          aspernatur. Aspernatur expedita a ea non iure aperiam assumenda
-          praesentium beatae numquam repellat, excepturi impedit voluptatem!
+        <Text fontSize="md">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Text>
       </Stack>
     </Flex>
