63f75b5ecbb23089064110b3db7f6f1069fece74
bbluechip <cs.orhan.berk@gmail.com>
2024-01-04 21:37:45 +0100

    trying the solve response.data.data issue and method issue

--- packages/ui/src/examples/E2_FetchWithStrapiRequest.tsx
+++ packages/ui/src/examples/E2_FetchWithStrapiRequest.tsx
@@ -7,7 +7,6 @@ import { strapiRequest } from '@wsvvrijheid/lib'
 const BLOG_URL = `https://wsvv-api-staging.onrender.com/api/blogs?locale=tr`
 const PUBLIC_TOKEN = '2ff0613e2f32eae88e6dbdb8f806d8b1bdbb7fab7b1b9c2f693013220fceae7423a1eb8fdf06938c3bd1afd8e695364e883434d714b223b0a2fb585fcd06432f105d80ff90be73f59d2fc4690919c8737e75b0f786b19f4cc6454e638ea77cefe3675d592580eb77656dcfcdb8c1042d599313e297012703ebf3ecf1d6e30603'
 
-
 interface Blog {
   id: number;
   title: string;
@@ -28,7 +27,12 @@ export const FetchWithStrapiRequest = () => {
           },
         })
 
-        setBlogs(response.data.data)
+        // Adjust the way you handle the API response based on the StrapiModel type
+        if ('data' in response.data) {
+          setBlogs(response.data.data as Blog[])
+        } else {
+          setBlogs([response.data as Blog])
+        }
       } catch (error) {
         console.error('Error fetching data:', error)
       }
