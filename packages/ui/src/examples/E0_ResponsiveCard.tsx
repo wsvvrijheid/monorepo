23c8a8b58d789ed91e7fb9b9c45a45722b9f3413
fatih <ermissaim@gmail.com>
2024-01-07 16:37:43 +0100

    create responsive card

--- packages/ui/src/examples/E0_ResponsiveCard.tsx
+++ packages/ui/src/examples/E0_ResponsiveCard.tsx
@@ -4,28 +4,42 @@ export const ResponsiveCard = () => {
   const randomPic = 'https://picsum.photos/600/400'
 
   return (
-    <Flex>
-      <Stack>
-        <Image src={randomPic} alt="random pic" />
+    <Flex shadow="lg" direction={{ base: 'column', sm: 'row' }}>
+      <Stack
+        w={{ base: '100%', sm: '30%' }}
+        display="flex"
+        justify="center"
+        align="center"
+      >
+        <Image
+          boxSize={{ base: '2xs', sm: 'xs' }}
+          objectFit={{ base: 'none', sm: 'cover' }}
+          borderRadius={{ base: 'full', sm: 'none' }}
+          p={1}
+          src={randomPic}
+          alt="random pic"
+        />
       </Stack>
 
-      <Stack>
-        <Heading as="h1" size="lg" mb={5} color="#000" fontWeight="500">
+      <Stack w={{ base: '100%', sm: '70%' }} p={2}>
+        <Heading
+          as="h1"
+          size="lg"
+          mb={4}
+          color="#000"
+          fontWeight="500"
+          textAlign={{ base: 'center', sm: 'inherit' }}
+        >
           This is Heading...
         </Heading>
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          dolores totam blanditiis soluta quae delectus doloremque velit sit
-          corrupti iure natus nobis nesciunt, tempore repellat cupiditate culpa
-          voluptatum expedita. Ducimus sapiente exercitationem repellendus et
-          aspernatur. Aspernatur expedita a ea non iure aperiam assumenda
-          praesentium beatae numquam repellat, excepturi impedit voluptatem!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          dolores totam blanditiis soluta quae delectus doloremque velit sit
-          corrupti iure natus nobis nesciunt, tempore repellat cupiditate culpa
-          voluptatum expedita. Ducimus sapiente exercitationem repellendus et
-          aspernatur. Aspernatur expedita a ea non iure aperiam assumenda
-          praesentium beatae numquam repellat, excepturi impedit voluptatem!
+        <Text p={1.5}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </Text>
       </Stack>
     </Flex>
