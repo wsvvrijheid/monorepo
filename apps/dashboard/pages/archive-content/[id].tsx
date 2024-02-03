dc13df0094b05a7654f5f383df82a8222224fefa
Hakan <s.hakanayata@gmail.com>
2024-02-03 13:50:34 +0100

    archive fetch func add

--- apps/dashboard/pages/archive-content/[id].tsx
+++ apps/dashboard/pages/archive-content/[id].tsx
@@ -2,12 +2,25 @@ import React from 'react'
 
 import { useRouter } from 'next/router'
 
+import { useStrapiRequest } from '@wsvvrijheid/services'
+import { ArchiveContent } from '@wsvvrijheid/types/src/archive-content'
+
 
 export default function ArchiveContent() {
-  const router = useRouter()
-  const id = router.query.id
+  const { locale, query } = useRouter()
+  const id = query.id
+
+  const archiveContentQuery = useStrapiRequest<ArchiveContent>({
+    endpoint: 'archive-contents',
+    locale,
+  })
 
   return (
-    <div>ArchiveContent {id}</div>
+    <div>
+      <h1>ArchiveContent {id}</h1>
+      {archiveContentQuery.data?.data.map((archive, idx) => {
+        return <p key={idx}>{archive.content}</p>
+      })}
+    </div>
   )
 }
