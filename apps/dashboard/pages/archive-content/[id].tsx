bddd14fd970465dd504f48007aaa73617f1616a7
Hakan <s.hakanayata@gmail.com>
2024-02-04 21:58:03 +0100

    style: substituted Stack for div

--- apps/dashboard/pages/archive-content/[id].tsx
+++ apps/dashboard/pages/archive-content/[id].tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { Heading, Stack } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 
 import { useStrapiRequest } from '@wsvvrijheid/services'
@@ -10,21 +11,24 @@ import { ArchivePostGenAI } from '@wsvvrijheid/ui'
 
 export default function ArchiveContent() {
   const { locale, query } = useRouter()
-  const id = query.id
+  const archiveContentId = query.id
 
   const archiveContentQuery = useStrapiRequest<ArchiveContent>({
     endpoint: 'archive-contents',
+    filters: {
+      id: { $eq: archiveContentId }
+    },
     locale,
   })
 
   const archiveContentData = archiveContentQuery.data?.data[0]
 
   return (
-    <div>
-      <h1>ArchiveContent {id}</h1>
+    <Stack p={2}>
+      <Heading mt={1}>Archive Content / {archiveContentId}</Heading>
       {archiveContentData?.id &&
         <ArchivePostGenAI archiveContentId={archiveContentData.id} content={archiveContentData.content} />
       }
-    </div>
+    </Stack>
   )
 }
