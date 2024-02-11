8bca050a163906edb6e53f443623481938178388
Hakan <s.hakanayata@gmail.com>
2024-02-11 20:36:35 +0100

    yarn format

--- packages/ui/src/data/tables/category.tsx
+++ packages/ui/src/data/tables/category.tsx
@@ -6,32 +6,31 @@ import { Category, Art } from '@wsvvrijheid/types'
 import { PublicationBadges } from '../../admin'
 import { WTableProps } from '../../components'
 
-export const useCategoryColumns =
-  (): WTableProps<Category>['columns'] => {
-    const { locale } = useRouter()
+export const useCategoryColumns = (): WTableProps<Category>['columns'] => {
+  const { locale } = useRouter()
 
-    return {
-      id: { sortable: true },
-      slug: { sortable: true },
-      arts: {
-        transform: value => (
-          <Wrap>
-            {(value as Art[])
-              .sort((a, b) =>
-                a[`title_${locale}`].localeCompare(b[`title_${locale}`]),
-              )
-              .map(c => (
-                <Badge variant={'outline'} key={c.id}>
-                  {c[`title_${locale}`]}
-                </Badge>
-              ))}
-          </Wrap>
-        ),
-      },
-      publishedAt: {
-        transform: value => (
-          <PublicationBadges publishedAt={value as string | null} />
-        ),
-      },
-    }
+  return {
+    id: { sortable: true },
+    slug: { sortable: true },
+    arts: {
+      transform: value => (
+        <Wrap>
+          {(value as Art[])
+            .sort((a, b) =>
+              a[`title_${locale}`].localeCompare(b[`title_${locale}`]),
+            )
+            .map(c => (
+              <Badge variant={'outline'} key={c.id}>
+                {c[`title_${locale}`]}
+              </Badge>
+            ))}
+        </Wrap>
+      ),
+    },
+    publishedAt: {
+      transform: value => (
+        <PublicationBadges publishedAt={value as string | null} />
+      ),
+    },
   }
+}
--- packages/ui/src/data/tables/tag.tsx
+++ packages/ui/src/data/tables/tag.tsx
@@ -3,16 +3,14 @@ import { Tag } from '@wsvvrijheid/types'
 import { PublicationBadges } from '../../admin'
 import { WTableProps } from '../../components'
 
-export const useTagColumns =
-  (): WTableProps<Tag>['columns'] => {
-
-    return {
-      id: { sortable: true },
-      slug: { sortable: true },
-      publishedAt: {
-        transform: value => (
-          <PublicationBadges publishedAt={value as string | null} />
-        ),
-      },
-    }
+export const useTagColumns = (): WTableProps<Tag>['columns'] => {
+  return {
+    id: { sortable: true },
+    slug: { sortable: true },
+    publishedAt: {
+      transform: value => (
+        <PublicationBadges publishedAt={value as string | null} />
+      ),
+    },
   }
+}
