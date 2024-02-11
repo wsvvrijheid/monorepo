11e4317fd66163d0d3a46c6c67a79f9da684ea5f
Hakan <s.hakanayata@gmail.com>
2024-02-11 21:51:13 +0100

    name fields add

--- packages/ui/src/data/tables/tag.tsx
+++ packages/ui/src/data/tables/tag.tsx
@@ -7,6 +7,9 @@ export const useTagColumns = (): WTableProps<Tag>['columns'] => {
   return {
     id: { sortable: true },
     slug: { sortable: true },
+    name_en: { sortable: true },
+    name_nl: { sortable: true },
+    name_tr: { sortable: true },
     publishedAt: {
       transform: value => (
         <PublicationBadges publishedAt={value as string | null} />
