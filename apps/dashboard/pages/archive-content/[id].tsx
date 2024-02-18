ce6c72c2dac371058f893c4b86352c8b5e7dcdc3
7alip <talipaltas@gmail.com>
2024-02-19 00:19:27 +0100

    Fix archive content detail page

--- apps/dashboard/pages/archive-content/[id].tsx
+++ apps/dashboard/pages/archive-content/[id].tsx
@@ -1,54 +1,36 @@
-import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
+import { Stack } from '@chakra-ui/react'
+import { GetServerSidePropsContext } from 'next'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 
-import { strapiRequest } from '@wsvvrijheid/lib'
 import { useStrapiRequest } from '@wsvvrijheid/services'
 import { ssrTranslations } from '@wsvvrijheid/services/ssrTranslations'
 import { StrapiLocale } from '@wsvvrijheid/types'
 import { ArchiveContent } from '@wsvvrijheid/types/src/archive-content'
 import { AdminLayout, ArchivePostGenAI } from '@wsvvrijheid/ui'
 
-type Props = InferGetServerSidePropsType<typeof getServerSideProps>
-
-export default function ArchiveContent({ categories }: Props) {
+export default function ArchiveContent() {
   const { t } = useTranslation()
-  const { locale } = useRouter()
+  const { query } = useRouter()
+  const id = query.id ? +query.id : 0
 
-  const archiveContentsQuery = useStrapiRequest<ArchiveContent>({
+  const archiveContentQuery = useStrapiRequest<ArchiveContent>({
     endpoint: 'archive-contents',
-    filters: {
-      categories: { $in: categories?.map(c => c.id) },
-    },
-    locale,
+    id,
   })
 
-  const archiveContents = archiveContentsQuery.data?.data || []
+  const archiveContentData = archiveContentQuery.data?.data
 
   return (
     <AdminLayout seo={{ title: t('archive-contents') }}>
-      <Tabs isLazy>
-        <TabList>
-          {archiveContents.map((archiveContent, index) => (
-            <Tab key={archiveContent.id}>
-              <Box fontWeight={700} noOfLines={1} maxW={200}>
-                {index + 1}. {archiveContent.source.repeat(10)}
-              </Box>
-            </Tab>
-          ))}
-        </TabList>
-        <TabPanels>
-          {archiveContents.map(archiveContent => (
-            <TabPanel key={archiveContent.id}>
-              <ArchivePostGenAI
-                archiveContentId={archiveContent.id}
-                content={archiveContent.content}
-              />
-            </TabPanel>
-          ))}
-        </TabPanels>
-      </Tabs>
+      <Stack>
+        {archiveContentData?.id && (
+          <ArchivePostGenAI
+            archiveContentId={archiveContentData.id}
+            content={archiveContentData.content}
+          />
+        )}
+      </Stack>
     </AdminLayout>
   )
 }
@@ -57,18 +39,9 @@ export const getServerSideProps = async (
   context: GetServerSidePropsContext,
 ) => {
   const locale = context.locale as StrapiLocale
-  const id = context.params?.id ? +context.params?.id : 0
-
-  const archiveContentResponse = await strapiRequest<ArchiveContent>({
-    endpoint: 'archive-contents',
-    id,
-  })
-
-  const categories = archiveContentResponse.data?.categories || []
 
   return {
     props: {
-      categories,
       ...(await ssrTranslations(locale)),
     },
   }
