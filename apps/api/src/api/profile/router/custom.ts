8c1bd66909ac9bb4467446043bd338ac92203ce9
bbluechip <cs.orhan.berk@gmail.com>
2024-02-28 19:01:06 +0100

    added comments

--- apps/api/src/api/profile/router/custom.ts
+++ apps/api/src/api/profile/router/custom.ts
@@ -5,6 +5,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 // I get an error while I have trying to fetch profile information, am I using the right function? I cannot import this function. 
 import { getSessionUser } from '../../../../../../packages/services/src/auth/getSessionUser';
 
+// Should I import getAuth here? 
+
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     res.status(405).json({ message: 'Method Not Allowed' });
@@ -12,6 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   try {
+    // how can I add outomated token here? 
     const userToken = 'burayaKullanıcınınTokenBilgisi';
     const user = await getSessionUser(userToken);
 
